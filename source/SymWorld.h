45b47f66c299db499422097f874fc742bb2d11da
Barbara Johnson <barbarajohnson@Barbaras-MacBook-Pro.local>
2017-08-14 11:12:17 -0500

    Testing makefile

--- source/SymWorld.h
+++ source/SymWorld.h
@@ -11,7 +11,8 @@ class SymWorld {
  public:
   SymWorld() {
     world.ConfigPop(20,20);
-    world.Insert( Host(0.5, Symbiont(), std::set<int>(), 0.0), 5 );
+    // changing 0.5 to 1.0 and 0.0 to 1.0 to see if I understand where these numbers go
+    world.Insert( Host(1.0, Symbiont(), std::set<int>(), 1.0), 5 );
     world.Print(PrintOrg);
   }
 
--- source/native/symbulation.cc
+++ source/native/symbulation.cc
@@ -8,6 +8,7 @@ int main()
 
   SymWorld world;
 
+
   
 
 }
