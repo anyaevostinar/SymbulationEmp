36d6175dc938508294a12fff912f7b8a26a134e3
Anya Johnson <anyaejo@gmail.com>
2017-09-26 14:49:04 -0500

    in process of adding configuration set up, not working yet

--- source/native/symbulation.cc
+++ source/native/symbulation.cc
@@ -2,7 +2,19 @@
 
 #include <iostream>
 #include "../SymWorld.h"
+#include "config/ArgManager.h"
 using namespace std;
+
+EMP_BUILD_CONFIG( SymConfigBase,
+		  VALUE(SEED, double, 10, "What value should the random seed be?"),
+		  VALUE(MUTATION_RATE, double, 0.001, "Standard deviation of the distribution to mutate by"),
+		  VALUE(SYNERGY, double, 5, "Amount symbiont's returned resources should be multiplied by"),
+		  VALUE(VERTICAL_TRANSMISSION, double, 1, "Value 0 to 1 of probability of symbiont vertically transmitting when host reproduces"),
+		  VALUE(GRID_X, int, 100, "Width of the world"),
+		  VALUE(GRID_Y, int, 100, "Height of world"),
+		  VALUE(UPDATES, int, 1000, "Number of updates to run before quitting")
+		  )
+		  
 
 
 void DistribResources(int resources) { // might want to declare a remainingResources variable just to make this easier to maintain
@@ -78,45 +90,58 @@ void DistribResources(int resources) { // might want to declare a remainingResou
 }
 	
 
-int main()
+int main(int argc, char * argv[])
 {
+
+ 
+  SymConfigBase config;
+
+  //config settings can be accessed by config.KEYWORD_NAME()
+  //ex: double seedf = config.SEED();
 
   SymWorld world; 
 
   int ControlOption = 0;
   int updateRounds = 1;
+  auto args = emp::cl::ArgManager(argc, argv);
+  if (args && args[0] == "r") {
+    cout << "Research Mode Go!" << endl;
+    config.Read("SymSettings.cfg");
+    if (args.ProcessConfigOptions(config, std::cout, "SymSettings.cfg") == false) exit(1);
+    if (args.TestUnknown() == false) exit(2);  // If there are leftover args, throw an error.
+    world.Update(config.UPDATES());
+  } else if {
+    do {
+      cout << endl;
+      cout << "Options: " << endl;
+      cout << "1 - Configure settings." << endl;
+      cout << "2 - Run updates for one (or more) generations." << endl;
+      cout << "3 - Print the world." << endl;
+      cout << "4 - Test resource behavior cases. " << endl;
+      cout << "5 - Print reports." << endl;
+      cout << "9 - Quit." << endl;
+      cin >> ControlOption;
   
-  do {
-  cout << endl;
-  cout << "Options: " << endl;
-  cout << "1 - Set configurations." << endl;
-  cout << "2 - Run updates for one (or more) generations." << endl;
-  cout << "3 - Print the world." << endl;
-  cout << "4 - Test resource behavior cases. " << endl;
-  cout << "5 - Print reports." << endl;
-  cout << "9 - Quit." << endl;
-  cin >> ControlOption;
-  
-  if (ControlOption == 1) {
-  	cout << "Not ready for user configuration yet!" << endl;
-  } else if (ControlOption == 2) {
-  	 cout << "How many generations? " << endl;
-  	 cin >> updateRounds;
-  	 for (int i = 0; i < updateRounds; i++) {
-       world.Update(10);
-       }
-  } else if (ControlOption == 3) {
-     world.PrintIt();
-  } else if (ControlOption == 4) {
-     DistribResources(20);
-  } else if (ControlOption == 5) {
-  	 cout << "Reports not ready yet." << endl;
-  }
-  else if (ControlOption == 9) {
-     continue;
-  } else {
-    cout << "Invalid choice!!" << endl;
-  }
-  
-   } while (ControlOption != 9) ;
+      if (ControlOption == 1) {
+	cout << "No controls yet for you" << endl;
+      } else if (ControlOption == 2) {
+	cout << "How many generations? " << endl;
+	cin >> updateRounds;
+	for (int i = 0; i < updateRounds; i++) {
+	  world.Update(10);
+	}
+      } else if (ControlOption == 3) {
+	world.PrintIt();
+      } else if (ControlOption == 4) {
+	DistribResources(20);
+      } else if (ControlOption == 5) {
+	cout << "Reports not ready yet." << endl;
+      }
+      else if (ControlOption == 9) {
+	continue;
+      } else {
+	cout << "Invalid choice!!" << endl;
+      }
+      
+    } while (ControlOption != 9) ;
 }
