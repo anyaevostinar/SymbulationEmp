20f38f79d4f3d16ddc31630c79bb8a665118ab7d
Lilith Hafner <60898866+LilithHafner@users.noreply.github.com>
2020-05-13 10:07:28 -0500

    Added documentation for testing via catch and for the data_management system

--- source/catch/main.cc
+++ source/catch/main.cc
@@ -2,7 +2,7 @@
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
 
-#include "../sanity_check.test.cc"
+#include "sanity_check.test.cc"
 #include "../SymOrg.test.cc"
 #include "../SymWorld.test.cc"
 #include "../end_to_end.test.cc"
