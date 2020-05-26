a311f0b949e0a880e5de521357e81ea09c357d13
Anya Johnson <anyaejo@gmail.com>
2020-05-26 11:11:42 -0500

    set up simple_repeat.py to work for vertical transmission rates

--- stats_scripts/simple_repeat.py
+++ stats_scripts/simple_repeat.py
@@ -1,8 +1,9 @@
 #Simplified versino of create_graphs.py
 
-seeds = range(10, 16)
-start_mois = [1, 5, 10, 15]
-slrs = [0.3, 1, 2.7, 3]
+seeds = range(10, 21)
+#start_mois = [1, 5, 10, 15]
+#slrs = [0.3, 1, 2.7, 3]
+verts = [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
 
 import subprocess
 
@@ -19,8 +20,8 @@ def silent_cmd(command):
     return subprocess.Popen(command, shell=True, stdout=subprocess.PIPE).wait()
 
 for a in seeds:
-    for b in start_mois:
-        for c in slrs:
-            command_str = './symbulation -SEED '+str(a)+' -START_MOI '+str(b)+' -SYM_LYSIS_RES '+str(c)+' -FILE_NAME SLR'+str(c)+'_SM'+str(b)+'_Seed'+str(a)
-            print(command_str)
-            silent_cmd('cd ..; '+command_str)
+    for b in verts:
+        #            command_str = './symbulation -SEED '+str(a)+' -START_MOI '+str(b)+' -SYM_LYSIS_RES '+str(c)+' -FILE_NAME SLR'+str(c)+'_SM'+str(b)+'_Seed'+str(a)
+        command_str = './symbulation -SEED '+str(a)+' -VERTICAL_TRANSMISSION '+str(b)+' -FILE_NAME _VT'+str(b)+'_Seed'+str(a)
+        print(command_str)
+        silent_cmd(command_str)
