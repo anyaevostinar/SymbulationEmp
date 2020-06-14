6a32919e687e372b1bd43fbe3bd038f39e0d5e36
Yi Qin <yiqin@Yis-MacBook.local>
2020-06-14 15:09:46 -0500

    Draw a virtual petri dish using canvas

--- source/web/symbulation-web.cc
+++ source/web/symbulation-web.cc
@@ -2,6 +2,8 @@
 #include "../SymWorld.h"
 #include "../../Empirical/source/config/ArgManager.h"
 #include "web/web.h"
+#include "web/d3/visualizations.h"
+#include "web/Document.h"
 
 using namespace std;
 
@@ -35,18 +37,29 @@ UI::Table my_table(10, 2, "my_table");
 UI::Document doc("emp_base");
 
 
-int main(){
+int symbulation_main(int argc, char * argv[]){
+    //n_objects(); // some line added to force it to call in order to fix the "js doesn't exist" problem.
     SymConfigBase config;
     config.Read("SymSettings.cfg");
-    if (config.BURST_SIZE()%config.BURST_TIME() != 0 && config.BURST_SIZE() < 999999999) {
-  	cerr << "BURST_SIZE must be an integer multiple of BURST_TIME." << endl;
-  	exit(1);
+    auto args = emp::cl::ArgManager(argc, argv);
+    if (args.ProcessConfigOptions(config, std::cout, "SymSettings.cfg") == false) {
+      cerr << "There was a problem in processing the options file." << endl;
+      exit(1);
     }
+    if (args.TestUnknown() == false) {
+      cerr << "Leftover args no good." << endl;
+      exit(1);
+    }
+    if (config.BURST_SIZE()%config.BURST_TIME() != 0 && config.BURST_SIZE() < 999999999) {
+      cerr << "BURST_SIZE must be an integer multiple of BURST_TIME." << endl;
+      exit(1);
+    }
+
     // params
     int numupdates = config.UPDATES();
     int start_moi = config.START_MOI();
     double POP_SIZE = config.GRID_X() * config.GRID_Y();
-    bool random_phen_host = false;
+    bool random_phen_host = true;
     bool random_phen_sym = false;
     if(config.HOST_INT() == -2) random_phen_host = true;
     if(config.SYM_INT() == -2) random_phen_sym = true;
@@ -56,17 +69,73 @@ int main(){
     SymWorld world(random); // create the world
     if (config.GRID() == 0) world.SetPopStruct_Mixed(); // needed on UI. Assume it's mixed offspring
     else world.SetPopStruct_Grid(config.GRID_X(), config.GRID_Y());
+    // settings
+    world.SetVertTrans(config.VERTICAL_TRANSMISSION());
+    world.SetMutRate(config.MUTATION_RATE());
+    world.SetSymLimit(config.SYM_LIMIT());
+    world.SetLysisBool(config.LYSIS());
+    world.SetHTransBool(config.HORIZ_TRANS());
+    world.SetBurstSize(config.BURST_SIZE());
+    world.SetBurstTime(config.BURST_TIME());
+    world.SetHostRepro(config.HOST_REPRO_RES());
+    world.SetSymHRes(config.SYM_HORIZ_TRANS_RES());
+    world.SetSymLysisRes(config.SYM_LYSIS_RES());
+    world.SetSynergy(config.SYNERGY());
+    world.SetResPerUpdate(100); // number of resources distributed per update
 
+    int TIMING_REPEAT = config.DATA_INT();
+    const bool STAGGER_STARTING_BURST_TIMERS = true;
+
+    //inject organisms
+    for (size_t i = 0; i < POP_SIZE; i++){
+      Host *new_org;
+      if (random_phen_host) new_org = new Host(random.GetDouble(-1, 1)); // Want random host genomes
+      else new_org = new Host(config.HOST_INT()); // want predefined host genomes
+          world.Inject(*new_org); // empirical-provided tool to put our host into the world
+
+      for (int j = 0; j < start_moi; j++){ 
+        Symbiont new_sym; 
+        if(random_phen_sym) new_sym = *(new Symbiont(random.GetDouble(-1, 1)));
+        else new_sym = *(new Symbiont(config.SYM_INT()));
+        if(STAGGER_STARTING_BURST_TIMERS)
+          new_sym.burst_timer = random.GetInt(-5,5);
+        world.InjectSymbiont(new_sym); 
+      }
+    }
+
+    // Drawing a virtual petri dish according to population size and color cells by intVal(interaction value)
     auto p = world.getPop();
+    constexpr int RECT_WIDTH = 15;
+    int side_x = config.GRID_X();
+    int side_y = config.GRID_Y();
+    for (size_t i = 0; i < p.size(); i++) doc << p[i]->GetIntVal() << " ";
+    doc << "</br>";
+    //auto hostCanvas = doc.AddCanvas(side_x * RECT_WIDTH, side_y * RECT_WIDTH, "can"); 
+    auto hostCanvas = doc.AddCanvas(side_x * RECT_WIDTH, side_y * RECT_WIDTH, "can"); // weird behavior of canvas. Have to do +1
+    for (int x = 0; x < side_x; x++){ // now draw a virtual petri dish. 20 is the starting coordinate
+      for (int y = 0; y < side_y; y++){
+        std::string color;
+        if (p[y]->GetIntVal() < 0) color = "blue";
+        else color = "yellow";
+        hostCanvas.Rect(20 + x * RECT_WIDTH, 20 + y * RECT_WIDTH, RECT_WIDTH, RECT_WIDTH, color, "black");
+      }
+    }
+
     // for (Host *h : p){
     //     double k = h->GetPoints();
     // }
     // Testing
-    my_table.GetCell(0, 1) << !p[0]->HasSym();
-    my_table.GetCell(0, 0) << "1st org:  ";
-    std::cout << p[0]->GetPoints() << std::endl;
-    doc << "<h1>Symbulation Project</h1>";
-    doc << my_table;
+    // my_table.GetCell(0, 1) << !p[0]->HasSym();
+    // my_table.GetCell(0, 0) << "1st org:  ";
+    // std::cout << p[0]->GetPoints() << std::endl;
+    // doc << "<h1>Symbulation Project</h1>";
+    // doc << my_table;
 
     return 0;
 }
+
+#ifndef CATCH_CONFIG_MAIN
+int main(int argc, char * argv[]) {
+  return symbulation_main(argc, argv);
+}
+#endif
