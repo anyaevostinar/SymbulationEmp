fb2188df51a0f2e1e4aa13a3f3b8d9a199029102
Anya Vostinar <anya.vostinar@gmail.com>
2021-01-15 12:11:44 -0600

    put unit tests back in

--- source/catch/main.cc
+++ source/catch/main.cc
@@ -3,7 +3,7 @@
 
 #include "catch.hpp"
 
-//#include "sanity_check.test.cc"
-//#include "../SymOrg.test.cc"
-//#include "../SymWorld.test.cc"
+#include "sanity_check.test.cc"
+#include "../SymOrg.test.cc"
+#include "../SymWorld.test.cc"
 #include "../end_to_end.test.cc"
