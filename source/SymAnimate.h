741e98731665c96f0580367c92e64d7728080e4b
Anya Vostinar <anya.vostinar@gmail.com>
2021-06-07 21:48:47 -0500

    fixed problems with configuration panel

--- source/SymAnimate.h
+++ source/SymAnimate.h
@@ -19,16 +19,21 @@
 
 
 namespace UI = emp::web;
+SymConfigBase config; // load the default configuration
+emp::prefab::ConfigPanel config_panel(config);
+
 
 class SymAnimate : public UI::Animate {
 private:
-  SymConfigBase config; // load the default configuration
+
   UI::Document animation;
   UI::Document settings;
+  UI::Document explanation;
+  UI::Document learnmore;
+  UI::Document buttons;
+  UI::Canvas mycanvas;
 
   const int RECT_WIDTH = 10;
-  size_t POP_SIDE;
-  int can_size;
   
   emp::Random random{config.SEED()};
   SymWorld world{random};
@@ -42,25 +47,45 @@ private:
 
 public:
 
-  SymAnimate() : animation("emp_animate"), settings("emp_settings") {
-    // Set parameters for "animation" and "settings" div to enable flex box
+  SymAnimate() : animation("emp_animate"), settings("emp_settings"), explanation("emp_explanation"), learnmore("emp_learnmore"), buttons("emp_buttons") {
 
     config.GRID_X(50); 
     config.GRID_Y(50); 
     config.UPDATES(30000);
+
+    //Exclude all the settings that control 
+    //things that don't show up in the GUI correctly
+    config_panel.ExcludeConfig("SYM_LIMIT"); 
+    config_panel.ExcludeConfig("DATA_INT");
+    config_panel.ExcludeConfig("POP_SIZE");
+    config_panel.ExcludeConfig("LYSIS");
+    config_panel.ExcludeConfig("BURST_SIZE");
+    config_panel.ExcludeConfig("BURST_TIME");
+    config_panel.ExcludeConfig("SYM_LYSIS_RES");
+    config_panel.ExcludeConfig("START_MOI");
+    config_panel.ExcludeConfig("FILE_PATH");
+    config_panel.ExcludeConfig("FILE_NAME");
+    config_panel.ExcludeConfig("COMPETITION_MODE");
+
     
 
     animation.SetCSS("flex-grow", "1");
     animation.SetCSS("max-width", "500px");
     settings.SetCSS("flex-grow", "1");
     settings.SetCSS("max-width", "600px");
+    explanation.SetCSS("flex-grow", "1");
+    explanation.SetCSS("max-width", "600px");
+    learnmore.SetCSS("flex-grow", "1");
+    learnmore.SetCSS("max-width", "600px");
+    buttons.SetCSS("flex-grow", "1");
+    buttons.SetCSS("max-width", "600px");
 
 
     initializeWorld();
     emp::prefab::Card config_panel_ex("INIT_CLOSED");
     settings << config_panel_ex;
-  config_panel_ex.AddHeaderContent("<h3>Settings</h3>");
-    emp::prefab::ConfigPanel config_panel(config);
+    config_panel_ex.AddHeaderContent("<h3>Settings</h3>");
+    
     // apply configuration query params and config files to config
     auto specs = emp::ArgManager::make_builtin_specs(&config);
     emp::ArgManager am(emp::web::GetUrlParams(), specs);
@@ -74,69 +99,69 @@ public:
 
 
     // Add explanation for organism color:
-    settings << "<br><br><img style=\"max-width:175px;\" src=\"diagram1.png\"> <br>" <<
+    explanation << "<br><br><img style=\"max-width:175px;\" src=\"diagram1.png\"> <br>" <<
       "<img style=\"max-width:600px;\" src = \"gradient1.png\"/> <br>";
 
 
     // ----------------------- Add a button that allows for pause and start toggle -----------------------
-    settings << "<br>";
-    settings.AddButton([this](){
+    buttons << "<br>";
+    buttons.AddButton([this](){
       // animate up to the number of updates
       ToggleActive();
-      auto but = settings.Button("toggle");
+      auto but = buttons.Button("toggle");
       if (GetActive()) but.SetLabel("Pause");
       else but.SetLabel("Start");
     }, "Start", "toggle");
     setButtonStyle("toggle");
-    settings.Button("toggle").OnMouseOver([this](){ auto but = settings.Button("toggle"); but.SetCSS("background-color", "grey"); but.SetCSS("cursor", "pointer"); });
-    settings.Button("toggle").OnMouseOut([this](){ auto but = settings.Button("toggle"); but.SetCSS("background-color", "#D3D3D3"); });
+    buttons.Button("toggle").OnMouseOver([this](){ auto but = buttons.Button("toggle"); but.SetCSS("background-color", "grey"); but.SetCSS("cursor", "pointer"); });
+    buttons.Button("toggle").OnMouseOut([this](){ auto but = buttons.Button("toggle"); but.SetCSS("background-color", "#D3D3D3"); });
 
     // ----------------------- Add a reset button to reset the animation/world -----------------------
     /* Note: Must first run world.Reset(), because Inject checks for valid position.
       If a position is occupied, new org is deleted and your world isn't reset.
       Also, canvas must be redrawn to let users see that it is reset */
-    settings.AddButton([this](){
+    buttons.AddButton([this](){
       world.Reset();
-      settings.Text("update").Redraw();
+      buttons.Text("update").Redraw();
       initializeWorld();
       p = world.getPop();
 
       if (GetActive()) { // If animation is running, stop animation and adjust button label
         ToggleActive();
       }
-      auto but = settings.Button("toggle");
+      auto but = buttons.Button("toggle");
       but.SetLabel("Start");
 
       // redraw petri dish
-      auto mycanvas = animation.Canvas("can");
+      mycanvas.SetWidth(RECT_WIDTH*config.GRID_X());
+      mycanvas.SetHeight(RECT_WIDTH*config.GRID_Y());
       drawPetriDish(mycanvas);
+      ToggleActive();//turn on quick to update the grid if the size changed
+      ToggleActive();//turn off again
     }, "Reset", "reset");
     setButtonStyle("reset");
-    settings.Button("reset").OnMouseOver([this](){ auto but = settings.Button("reset"); but.SetCSS("background-color", "grey"); but.SetCSS("cursor", "pointer"); });
-    settings.Button("reset").OnMouseOut([this](){ auto but = settings.Button("reset"); but.SetCSS("background-color", "#D3D3D3"); });
+    buttons.Button("reset").OnMouseOver([this](){ auto but = buttons.Button("reset"); but.SetCSS("background-color", "grey"); but.SetCSS("cursor", "pointer"); });
+    buttons.Button("reset").OnMouseOut([this](){ auto but = buttons.Button("reset"); but.SetCSS("background-color", "#D3D3D3"); });
 
     // ----------------------- Keep track of number of updates -----------------------
-    settings << "<br>";
-    settings << UI::Text("update") << "Update = " << UI::Live( [this](){ return world.GetUpdate(); } ) << "  ";
-    settings << UI::Text("mut") << "Mutualistic = " << UI::Live( [this](){ return num_mutualistic; } ) << "  ";
-    settings << UI::Text("par") << " Parasitic = " << UI::Live( [this](){ return num_parasitic; } );
-    settings << "<br>";
+    buttons << "<br>";
+    buttons << UI::Text("update") << "Update = " << UI::Live( [this](){ return world.GetUpdate(); } ) << "  ";
+    buttons << UI::Text("mut") << "Mutualistic = " << UI::Live( [this](){ return num_mutualistic; } ) << "  ";
+    buttons << UI::Text("par") << " Parasitic = " << UI::Live( [this](){ return num_parasitic; } );
+    buttons << "<br>";
 
     // Add a canvas for petri dish and draw the initial petri dish
-    POP_SIDE = (size_t) std::sqrt(config.GRID_X() * config.GRID_Y());
-    can_size = RECT_WIDTH * POP_SIDE; // set canvas size to be just enough to incorporate petri dish
-    auto mycanvas = animation.AddCanvas(can_size, can_size, "can");
+    mycanvas = animation.AddCanvas(RECT_WIDTH*config.GRID_X(), RECT_WIDTH*config.GRID_Y(), "can");
     targets.push_back(mycanvas);
     drawPetriDish(mycanvas);
     animation << "<br>";
 
-    settings << "If you'd like to learn more, please see the publication <a href=\"https://www.mitpressjournals.org/doi/abs/10.1162/artl_a_00273\">Spatial Structure Can Decrease Symbiotic Cooperation</a>.";
+    learnmore << "If you'd like to learn more, please see the publication <a href=\"https://www.mitpressjournals.org/doi/abs/10.1162/artl_a_00273\">Spatial Structure Can Decrease Symbiotic Cooperation</a>.";
 
   }
 
   void initializeWorld(){
      // Reset the seed and the random machine of world to ensure consistent result (??)
-     std::cout << "Grid x: " << config.GRID_X() << std::endl;
     random.ResetSeed(config.SEED());
     world.SetRandom(random);
 
@@ -147,7 +172,7 @@ public:
   }
 
   void setButtonStyle(std::string but_id){
-    auto but = settings.Button(but_id);
+    auto but = buttons.Button(but_id);
     but.SetCSS("background-color", "#D3D3D3");
     but.SetCSS("border-radius", "4px");
     but.SetCSS("margin-left", "5px");
@@ -155,10 +180,6 @@ public:
 
   // now draw a virtual petri dish with coordinate offset from the left frame
   void drawPetriDish(UI::Canvas & can){
-        // Params for controlling petri dish
-        
-        POP_SIDE = (size_t) std::sqrt(config.GRID_X() * config.GRID_Y());
-        can_size = RECT_WIDTH * POP_SIDE; // set canvas size to be just enough to incorporate petri dish
         int i = 0;
         num_mutualistic = 0;
         num_parasitic = 0;
@@ -170,15 +191,14 @@ public:
 
                 std::string color_host = matchColor(p[i]->GetIntVal());
                 
-                // while drawing, test whether every organism is mutualistic
-                if (p[i]->GetIntVal() <= 0) num_parasitic++;
-                else num_mutualistic++;
+                
                 
                 // Draw host rect and symbiont dot
                 can.Rect(x * RECT_WIDTH, y * RECT_WIDTH, RECT_WIDTH, RECT_WIDTH, color_host, "black");
                 int radius = RECT_WIDTH / 4;
                 if(syms.size() == 1) {
                   std::string color_sym = matchColor(syms[0]->GetIntVal());
+                  // while drawing, test whether every organism is mutualistic
                   if (syms[0]->GetIntVal() <= 0) num_parasitic++;
                   else num_mutualistic++;
                   can.Circle(x * RECT_WIDTH + RECT_WIDTH/2, y * RECT_WIDTH + RECT_WIDTH/2, radius, color_sym, "black");
@@ -222,16 +242,16 @@ public:
     if (world.GetUpdate() == config.UPDATES() && GetActive()) {
         ToggleActive();
     } else {
-      auto mycanvas = animation.Canvas("can"); // get canvas by id
+      mycanvas = animation.Canvas("can"); // get canvas by id
       mycanvas.Clear();
 
       // Update world and draw the new petri dish
       world.Update();
       p = world.getPop();
       drawPetriDish(mycanvas);
-      settings.Text("update").Redraw();
-      settings.Text("mut").Redraw();
-      settings.Text("par").Redraw();
+      buttons.Text("update").Redraw();
+      buttons.Text("mut").Redraw();
+      buttons.Text("par").Redraw();
     }
   }
 
--- source/web/symbulation-web.cc
+++ source/web/symbulation-web.cc
@@ -7,24 +7,10 @@
 using namespace std;
 
 namespace UI = emp::web;
-SymConfigBase config;
 SymAnimate anim;
 
 int symbulation_main(int argc, char * argv[]){
-    auto args = emp::cl::ArgManager(argc, argv);
-    if (args.ProcessConfigOptions(config, std::cout, "SymSettings.cfg") == false) {
-      cerr << "There was a problem in processing the options file." << endl;
-      exit(1);
-    }
-    if (args.TestUnknown() == false) {
-      cerr << "Leftover args no good." << endl;
-      exit(1);
-    }
-    if (config.BURST_SIZE()%config.BURST_TIME() != 0 && config.BURST_SIZE() < 999999999) {
-      cerr << "BURST_SIZE must be an integer multiple of BURST_TIME." << endl;
-      exit(1);
-    }
-    return 0;
+    
 }
 
 #ifndef CATCH_CONFIG_MAIN
