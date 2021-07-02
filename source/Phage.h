692e19682ede706936c16d9b36250399a38e2dba
piperwelch <welchp@carleton.edu>
2021-07-02 09:37:15 -0500

    fixing lines

--- source/Phage.h
+++ source/Phage.h
@@ -46,12 +46,14 @@ public:
     if(lysis && GetHost() != NULL) { //lysis enabled, checking for lysis
       if(GetBurstTimer() >= burst_time) { //time to lyse!
 
+        emp::vector<emp::Ptr<Organism>>& repro_syms = my_host->GetReproSymbionts();
+
         emp::DataMonitor<double>& data_node_burst_size = my_world->GetBurstSizeDataNode();
         data_node_burst_size.AddDatum(repro_syms.size());
         emp::DataMonitor<int>& data_node_burst_count = my_world->GetBurstCountDataNode();
         data_node_burst_count.AddDatum(1);
 
-        emp::vector<emp::Ptr<Organism>>& repro_syms = my_host->GetReproSymbionts();
+
         
         for(size_t r=0; r<repro_syms.size(); r++) {
           my_world->SymDoBirth(repro_syms[r], location);
