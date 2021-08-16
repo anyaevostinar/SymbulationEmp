ff720245d743df3a62e0a74d9181fb1dc06a21bd
dorchens <dorchens@carleton.edu>
2021-08-16 11:43:31 -0500

    changed genome to host_inc_val and fixed bacterium tests

--- source/Bacterium.test.cc
+++ source/Bacterium.test.cc
@@ -1,28 +1,28 @@
 #include "Bacterium.h"
 #include "Host.h"
 
-TEST_CASE("Bacterium constructor, random starter genome"){
+TEST_CASE("Bacterium constructor, host_incorporation_val"){
     emp::Ptr<emp::Random> random = new emp::Random(5);
     SymWorld w(*random);
     SymConfigBase config;
     SymWorld * world = &w;
     double int_val = -1;
 
-    config.GENOME_VAL(-1);
+    config.HOST_INC_VAL(-1);
     Bacterium * b = new Bacterium(random, world, &config, int_val);
-    double expected_genome_value = 0.7096757176;
-    REQUIRE(b->GetGenomeValue()==Approx(expected_genome_value));
+    REQUIRE(b->GetIncVal() >= 0.0);
+    REQUIRE(b->GetIncVal() <= 1.0);
 
-    config.GENOME_VAL(0.8);
+    config.HOST_INC_VAL(0.8);
     Bacterium * b2 = new Bacterium(random, world, &config, int_val);
-    expected_genome_value = 0.8;
-    REQUIRE(b2->GetGenomeValue()==Approx(expected_genome_value));
+    double expected_inc_val = 0.8;
+    REQUIRE(b2->GetIncVal()==expected_inc_val);
 
     delete b;
     delete b2;
 }
 
-TEST_CASE("Bacterium SetGenomeValue, GetGenomeValue"){
+TEST_CASE("Bacterium SetIncVal, GetIncVal"){
     emp::Ptr<emp::Random> random = new emp::Random(4);
     SymWorld w(*random);
     SymWorld * world = &w;
@@ -30,10 +30,10 @@ TEST_CASE("Bacterium SetGenomeValue, GetGenomeValue"){
     double int_val = -1;
     emp::Ptr<Bacterium> b = new Bacterium(random, world, &config, int_val);
 
-    double genome = 0.2;
-    b->SetGenomeValue(genome);
-    double expected_genome_value = 0.2;
-    REQUIRE(b->GetGenomeValue()==expected_genome_value);
+    double host_incorporation_val = 0.2;
+    b->SetIncVal(host_incorporation_val);
+    double expected_inc_val = 0.2;
+    REQUIRE(b->GetIncVal()==expected_inc_val);
 
 }
 
@@ -43,65 +43,63 @@ TEST_CASE("Bacterium mutate"){
     SymWorld * world = &w;
     SymConfigBase config;
     double int_val = 0;
-    double expected_genome_post_mutation = 0.0;
-    config.GENOME_VAL(0.5);
+    double orig_host_inc_val = 0.5;
+    config.HOST_INC_VAL(orig_host_inc_val);
     
 
-    WHEN("Mutation rate is not zero and genome mutations are enabled"){
+    WHEN("Mutation rate is not zero and host_inc_val mutations are enabled"){
         config.MUTATION_SIZE(0.002);
         config.MUTATION_RATE(1);
-        config.MUTATE_GENOME(1);
+        config.MUTATE_HOST_INC_VAL(1);
         emp::Ptr<Bacterium> b = new Bacterium(random, world, &config, int_val);
         b->mutate();
-        expected_genome_post_mutation = 0.4983370739;
 
-        THEN("Then mutation occurs and the bacterium's genome mutates"){
-            REQUIRE(b->GetGenomeValue() == Approx(expected_genome_post_mutation));
+        THEN("Then mutation occurs and the bacterium's host_inc_val mutates"){
+            REQUIRE(b->GetIncVal() != orig_host_inc_val);
+            REQUIRE(b->GetIncVal() >= 0.0);
+            REQUIRE(b->GetIncVal() <= 1.0);
         }
         
     delete b;
     }
 
-    WHEN("Mutation rate is not zero and genome mutations are not enabled"){
+    WHEN("Mutation rate is not zero and host_inc_val mutations are not enabled"){
         config.MUTATION_SIZE(0.002);
         config.MUTATION_RATE(1);
-        config.MUTATE_GENOME(0);
+        config.MUTATE_HOST_INC_VAL(0);
         emp::Ptr<Bacterium> b = new Bacterium(random, world, &config, int_val);
         b->mutate();
-        expected_genome_post_mutation = 0.5;
-
-        THEN("Then mutations occur but do not occur in the genome"){
-            REQUIRE(b->GetGenomeValue() == Approx(expected_genome_post_mutation));
+        
+        THEN("Then mutations occur but do not occur in the host_inc_val"){
+            REQUIRE(b->GetIncVal() ==  orig_host_inc_val);
         }
         
     delete b;
     }
 
-    WHEN("Mutation rate is 0 and genome mutations are enabled"){
+    WHEN("Mutation rate is 0 and host_inc_val mutations are enabled"){
         config.MUTATION_RATE(0.0);
         config.MUTATION_SIZE(0.0);
-        config.MUTATE_GENOME(1);
+        config.MUTATE_HOST_INC_VAL(1);
         emp::Ptr<Bacterium> b = new Bacterium(random, world, &config, int_val);
         b->mutate();
-        expected_genome_post_mutation = 0.5;
 
         THEN("Mutations do not occur"){
-            REQUIRE(b->GetGenomeValue() == Approx(expected_genome_post_mutation));
+            REQUIRE(b->GetIncVal() ==  orig_host_inc_val);
         }
 
     delete b;
     }
 
-    WHEN("Mutation rate is 0 and genome mutations are not enabled"){
+    WHEN("Mutation rate is 0 and host_inc_val mutations are not enabled"){
         config.MUTATION_RATE(0.0);
         config.MUTATION_SIZE(0.0);
-        config.MUTATE_GENOME(0);
+        config.MUTATE_HOST_INC_VAL(0);
         emp::Ptr<Bacterium> b = new Bacterium(random, world, &config, int_val);
         b->mutate();
-        expected_genome_post_mutation = 0.5;
 
         THEN("Mutations do not occur"){
-            REQUIRE(b->GetGenomeValue() == Approx(expected_genome_post_mutation));
+            REQUIRE(b->GetIncVal() ==  orig_host_inc_val);
         }
 
     delete b;
