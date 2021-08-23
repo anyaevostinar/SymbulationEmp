2167498aaa5494176c45b8719e500658b4947215
acameron13 <camerona2@carleton.edu>
2021-08-23 12:21:38 -0500

    Removed bacteria and phage incorporation val datafiles

--- source/native/symbulation.cc
+++ source/native/symbulation.cc
@@ -54,8 +54,6 @@ int symbulation_main(int argc, char * argv[])
   if (config.LYSIS() == 1) {
     world.SetupLysisChanceFile(config.FILE_PATH()+"LysisChance"+config.FILE_NAME()+file_ending).SetTimingRepeat(TIMING_REPEAT);
     world.SetupInductionChanceFile(config.FILE_PATH()+"InductionChance"+config.FILE_NAME()+file_ending).SetTimingRepeat(TIMING_REPEAT);
-    world.SetupPhageIncorporationFile(config.FILE_PATH()+"PhageIncVals"+config.FILE_NAME()+file_ending).SetTimingRepeat(TIMING_REPEAT);
-    world.SetupBacteriumIncorporationFile(config.FILE_PATH()+"BacteriumIncVals"+config.FILE_NAME()+file_ending).SetTimingRepeat(TIMING_REPEAT);
     world.SetupIncorporationDifferenceFile(config.FILE_PATH()+"IncValDifferences"+config.FILE_NAME()+file_ending).SetTimingRepeat(TIMING_REPEAT);
   }else if(config.PGG() == 1){
     world.SetupPGGSymIntValFile(config.FILE_PATH()+"PGGSymVals"+config.FILE_NAME()+file_ending).SetTimingRepeat(TIMING_REPEAT);
