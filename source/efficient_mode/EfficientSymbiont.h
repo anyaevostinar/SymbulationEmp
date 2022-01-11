87283e55d78222321244e68e0b53856baf9eb9e7
acameron13 <camerona2@carleton.edu>
2022-01-11 14:50:07 -0600

    removed instance variables that can be accessed through config

--- source/efficient_mode/EfficientSymbiont.h
+++ source/efficient_mode/EfficientSymbiont.h
@@ -55,12 +55,12 @@ public:
     efficiency = _efficient;
     my_world = _world;
     if(my_config->HORIZ_MUTATION_RATE() < 0){
-      ht_mut_rate = mut_rate;
+      ht_mut_rate = my_config->MUTATION_RATE();
     } else {
       ht_mut_rate = my_config->HORIZ_MUTATION_RATE();
     }
     if(my_config->HORIZ_MUTATION_SIZE() < 0) {
-      ht_mut_size = mut_size;
+      ht_mut_size = my_config->MUTATION_SIZE();
     } else {
       ht_mut_size = my_config->HORIZ_MUTATION_SIZE();
     }
@@ -135,8 +135,8 @@ public:
     double local_size;
     double local_rate;
     if(mode == "vertical"){
-      local_rate = mut_rate;
-      local_size = mut_size;
+      local_rate = my_config->MUTATION_RATE();
+      local_size = my_config->MUTATION_SIZE();
     } else if(mode == "horizontal") {
       local_rate = ht_mut_rate;
       local_size = ht_mut_size;
@@ -212,8 +212,8 @@ public:
    * Purpose: To check and allow for horizontal transmission to occur
    */
   void HorizontalTransmission(size_t location) {
-    if (h_trans) { //non-lytic horizontal transmission enabled
-      if(GetPoints() >= sym_h_res) {
+    if (my_config->HORIZ_TRANS()) { //non-lytic horizontal transmission enabled
+      if(GetPoints() >= my_config->SYM_HORIZ_TRANS_RES()) {
         // symbiont reproduces independently (horizontal transmission) if it has enough resources
         // new symbiont in this host with mutated value
         SetPoints(0); //TODO: test just subtracting points instead of setting to 0
