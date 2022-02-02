9078c21f3193e45b536c6f59fb5303fea96456be
Kiara Johnson <kiara@oneotaslopes.org>
2022-02-02 16:04:40 -0600

    restoring small phylogeny lines

--- source/default_mode/SymWorld.h
+++ source/default_mode/SymWorld.h
@@ -3,6 +3,7 @@
 
 #include "../../../Empirical/include/emp/Evolve/World.hpp"
 #include "../../../Empirical/include/emp/data/DataFile.hpp"
+#include "../../Empirical/include/emp/Evolve/Systematics.hpp"
 #include "../../../Empirical/include/emp/math/random_utils.hpp"
 #include "../../../Empirical/include/emp/math/Random.hpp"
 #include "../Organism.h"
