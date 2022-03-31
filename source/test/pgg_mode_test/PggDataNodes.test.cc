3513bbfb7504dc1b2a10ed4f2df4d2a0e1519ea1
Kiara Johnson <kiara@oneotaslopes.org>
2022-03-30 20:29:35 -0500

    Efficient & PGG data node basic tests

---
+++ source/test/pgg_mode_test/PggDataNodes.test.cc
@@ -0,0 +1,38 @@
+#include "../../pgg_mode/Pgghost.h"
+#include "../../pgg_mode/Pggsym.h"
+#include "../../pgg_mode/PggWorld.h"
+
+TEST_CASE("GetPGGDataNode", "[pgg]"){
+  GIVEN("a world"){
+    emp::Random random(17);
+    SymConfigBase config;
+    int int_val = 0;
+    PggWorld w(random);
+    w.Resize(4);
+    w.SetFreeLivingSyms(1);
+    config.SYM_LIMIT(3);
+
+    emp::DataMonitor<double, emp::data::Histogram>& sym_donation_node = w.GetPGGDataNode();
+    REQUIRE(std::isnan(sym_donation_node.GetMean()));
+
+    WHEN("efficient symbionts are added to the world"){
+      size_t num_syms = 6;
+      double donation_vals[6] = {0, 0.3, 0.45, 0.6, 0.77, 1.0};
+      double expected_av = 0.52;
+
+      Host *h = new PggHost(&random, &w, &config, int_val);
+      w.AddOrgAt(h, 0);
+
+      for(size_t i = 0; i < (num_syms/2); i++){
+        w.AddOrgAt(new PGGSymbiont(&random, &w, &config, int_val, donation_vals[i]), emp::WorldPosition(0, i));
+        h->AddSymbiont(new PGGSymbiont(&random, &w, &config, int_val, donation_vals[i+3]));
+      }
+
+      w.Update();
+      THEN("their efficiency is tracked by a data node"){
+        REQUIRE(sym_donation_node.GetMean() < (expected_av + 0.0001));
+        REQUIRE(sym_donation_node.GetMean() > (expected_av - 0.0001));
+      }
+    }
+  }
+}
