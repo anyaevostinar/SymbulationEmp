dc72dbfe7f2dc36313c176f15390ca5f0590cde1
Kiara Johnson <kiara@oneotaslopes.org>
2022-03-30 20:50:38 -0500

    adding mean IC data node tests

--- source/catch/main.cc
+++ source/catch/main.cc
@@ -7,6 +7,7 @@
 
 #include "../test/default_mode_test/SymWorld.test.cc"
 #include "../test/default_mode_test/DataNodes.test.cc"
+
 #include "../test/default_mode_test/Host.test.cc"
 #include "../test/default_mode_test/Symbiont.test.cc"
 #include "../test/default_mode_test/HostSymbiontInteraction.test.cc"
@@ -14,6 +15,7 @@
 
 #include "../test/efficient_mode_test/EfficientSymbiont.test.cc"
 #include "../test/efficient_mode_test/EfficientHost.test.cc"
+#include "../test/efficient_mode_test/EfficientDataNodes.test.cc"
 
 #include "../test/lysis_mode_test/Bacterium.test.cc"
 #include "../test/lysis_mode_test/Phage.test.cc"
@@ -23,6 +25,7 @@
 #include "../test/pgg_mode_test/Pggsym.test.cc"
 #include "../test/pgg_mode_test/Pgghost.test.cc"
 #include "../test/pgg_mode_test/PggHostPggSymUnitTest.test.cc"
+#include "../test/pgg_mode_test/PggDataNodes.test.cc"
 
 //#include "../Pggendtoend.test.cc"
 //#include "../test/end_to_end.test.cc"
