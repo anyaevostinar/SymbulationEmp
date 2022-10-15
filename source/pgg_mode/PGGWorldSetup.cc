aa018cec2a7607423a462a685c1d787981406227
Kiara Johnson <kiara@oneotaslopes.org>
2022-10-14 21:34:16 -0500

    restoring pgg changes lost in merge

--- source/pgg_mode/PGGWorldSetup.cc
+++ source/pgg_mode/PGGWorldSetup.cc
@@ -20,53 +20,45 @@ void PGGWorld::SetupHosts(long unsigned int* POP_SIZE){
   }
 }
 
-  //sets up the world size
-  world->Resize(my_config->GRID_X(), my_config->GRID_Y());
 
-  //This loop must be outside of the host generation loop since otherwise
-  //syms try to inject into mostly empty spots at first
-  int total_syms = POP_SIZE * start_moi;
-  for (int j = 0; j < total_syms; j++){
-      double sym_int = 0;
-      if (random_phen_sym) {sym_int = random.GetDouble(-1,1);}
-      else {sym_int = my_config->SYM_INT();}
-      
-      double sym_donation = 0;
+/**
+ * Input: The number of PGG symbionts and whether the PGG symbionts have random phenotypes.
+ *
+ * Output: None.
+ *
+ * Purpose: To populate the world with PGG symbionts with appropriate phenotypes.
+ */
+void PGGWorld::SetupSymbionts(long unsigned int* total_syms) {
+  for (size_t j = 0; j < *total_syms; j++) {
+	double sym_donation = 0;
 
-      //If the user wants the symbionts' donation rate to be randomly initialized
-      if (my_config->PGG_DONATE_RANDOM())
-      {
-	   //If the user wants the initialization of the symbionts' donation
-	   //rate to be selected using a normal distribution  
-	   if (my_config->PGG_DONATE_NORMAL())
-	   {   
-                double mean = my_config->PGG_DONATE_NORMAL_MEAN();
+	//If the user wants the symbionts' donation rate to be randomly initialized
+	if (my_config->PGG_DONATE_RANDOM()) {
+	  //If the user wants the initialization of the symbionts' donation
+	  //rate to be selected using a normal distribution  
+	  if (my_config->PGG_DONATE_NORMAL()) {
+		double mean = my_config->PGG_DONATE_NORMAL_MEAN();
 		double std = my_config->PGG_DONATE_NORMAL_STD();
 
-		sym_donation = random.GetRandNormal(mean, std);
+		sym_donation = random_ptr->GetRandNormal(mean, std);
 
 		//If selected value is out of the initialization range
-		if (sym_donation > my_config->PGG_DONATE_MAX())
-		{
-		     sym_donation = my_config->PGG_DONATE_MAX();
-		}	
-		else if (sym_donation < my_config->PGG_DONATE_MIN())
-		{
-		     sym_donation = my_config->PGG_DONATE_MIN();
-		}	
-	   }
-	   else
-	   {	   
-                sym_donation = random.GetDouble(my_config->PGG_DONATE_MIN(), my_config->PGG_DONATE_MAX());
-           }
-      }	      
-      else
-      {
-           sym_donation = my_config->PGG_DONATE();
-      }	      
-      emp::Ptr<PGGSymbiont> new_sym = emp::NewPtr<PGGSymbiont>(&random, world, my_config,
-          sym_int,sym_donation,0);
-      world->InjectSymbiont(new_sym);
+		if (sym_donation > my_config->PGG_DONATE_MAX()) {
+		  sym_donation = my_config->PGG_DONATE_MAX();
+		}
+		else if (sym_donation < my_config->PGG_DONATE_MIN()) {
+		  sym_donation = my_config->PGG_DONATE_MIN();
+		}
+	  }
+	  else {
+		sym_donation = random_ptr->GetDouble(my_config->PGG_DONATE_MIN(), my_config->PGG_DONATE_MAX());
+	  }
+	}
+	else {
+	  sym_donation = my_config->PGG_DONATE();
+	}
+	emp::Ptr<PGGSymbiont> new_sym = emp::NewPtr<PGGSymbiont>(&GetRandom(), this, my_config, my_config->SYM_INT(), sym_donation, 0);
+	InjectSymbiont(new_sym);
   }
 }
 
