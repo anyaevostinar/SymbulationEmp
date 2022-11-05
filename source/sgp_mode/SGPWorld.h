7fca6809e599e4a2ebf46f9b89f25509c4b34bcc
Kiara Johnson <kiara@oneotaslopes.org>
2022-11-05 13:32:48 -0500

    sgp org counts data file

--- source/sgp_mode/SGPWorld.h
+++ source/sgp_mode/SGPWorld.h
@@ -135,6 +135,7 @@ public:
   SyncDataMonitor<double> &GetSymEarnedDataNode();
   void SetupTasksNodes();
 
+  emp::DataFile &SetUpOrgCountFile(const std::string &filename);
   emp::DataFile &SetupSymDonatedFile(const std::string &filename);
   emp::DataFile &SetupTasksFile(const std::string &filename);
   emp::DataFile &SetupHostSquareFrequencyFile(const std::string &filename);
