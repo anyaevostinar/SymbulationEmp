4a526722c1d8a6c627e0c3376ccfca17302ab3f2
Anya Vostinar <anya.vostinar@gmail.com>
2022-12-14 11:42:52 -0600

    ousting data, last parasite build

--- Analysis/12-14-22-Ousting/TaskComp.R
+++ Analysis/12-14-22-Ousting/TaskComp.R
@@ -6,12 +6,11 @@ library(scales)
 #Set your working directory to the Analysis folder for your project
 
 #Read in the data
-initial_data <- read.table("12-12-22-LimitedRes/munged_basic.dat", h=T)
+initial_data <- read.table("12-14-22-Ousting/munged_basic.dat", h=T)
 initial_data$task <- factor(initial_data$task, levels=c("NOT", "NAND", "AND", "ORN", "OR", "ANDN", "NOR", "XOR", "EQU"))
-initial_data[2] <- data.frame(lapply(initial_data[2], function(x) {gsub("Long_MOI0.0", "Absent", x)}))
-initial_data[2] <- data.frame(lapply(initial_data[2], function(x) {gsub("Long_MOI1.0", "Present", x)}))
-#final_update <- subset(initial_data, update == "40000")
-final_update <- subset(initial_data, update == "79900")
+initial_data[2] <- data.frame(lapply(initial_data[2], function(x) {gsub("MOI0.0", "Absent", x)}))
+initial_data[2] <- data.frame(lapply(initial_data[2], function(x) {gsub("MOI1.0", "Present", x)}))
+final_update <- subset(initial_data, update == "40000")
 
 host_data <- subset(final_update, partner == "Host")
 
@@ -49,7 +48,7 @@ ggplot(data=limited_tasks, aes(x=update, y=count, group=treatment, colour=treatm
 
 # Specific parasite tasks over time
 parasite_over_time <- subset(initial_data, partner=="Parasite")
-para_limited_tasks <- subset(parasite_over_time, task=="NOT" | task=="NAND" | task=="AND" | task=="ORN" | task=="OR" | task=="ANDN")
+para_limited_tasks <- subset(parasite_over_time, task=="NOT" | task=="NAND" | task=="AND" | task=="ORN" | task=="OR")
 
 ggplot(data=para_limited_tasks, aes(x=update, y=count, group=treatment, colour=treatment)) + ylab("Task count") + xlab("Updates") + stat_summary(aes(color=treatment, fill=treatment),fun.data="mean_cl_boot", geom=c("smooth"), se=TRUE) + theme(panel.background = element_rect(fill='white', colour='black')) + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank()) + guides(fill=FALSE) +scale_colour_manual(name = "Parasites", values=viridis(3)) + scale_fill_manual(values=viridis(3)) + facet_grid(task ~ ., scales='free')
 
