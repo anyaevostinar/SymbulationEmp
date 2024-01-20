b06d0b4856ee74c95762c833dd5224eb26950b81
bennetthilberg <bennett.hilberg@gmail.com>
2024-01-20 11:51:14 -0600

    Fix React 17/18 error

--- web/index.js
+++ web/index.js
@@ -1,15 +1,18 @@
 import React, { useState } from 'react';
 import Tutorial from './tutorial';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 
-function ReactApp() {
+function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div id="reactAppRendered">
+    <div id="appRendered">
       <Tutorial />
     </div>
   );
 }
 
-ReactDOM.render(<ReactApp />, document.getElementById('reactApp'));
+//ReactDOM.render(<ReactApp />, document.getElementById('reactApp'));
+const root = ReactDOM.createRoot(document.getElementById('app'));
+root.render(<App />);
