00412c1c799379e3232a5a00ede3596850c1c329
bennetthilberg <bennett.hilberg@gmail.com>
2024-02-13 11:54:29 -0600

    Add sym-img-2-recropped and Symbionts tutorial step

--- web/tutorials.js
+++ web/tutorials.js
@@ -1,5 +1,7 @@
 import getToggleClickPromise from "./utilities/getToggleClickPromise"
 // advanceOn is a promise that, when resolved, will advance the tutorial to the next step.
+// https://zhixingj.github.io/SymbulationEmp/web/symbulation.html
+// ^^ that's the old tutorial with info to make steps out of.
 export const tutorials = [
   {
     id: 0,
@@ -20,8 +22,16 @@ export const tutorials = [
       },
       {
         titleText: 'Endosymbiosis',
-        bodyText: 'The symbionts are endosymbionts, meaning they live and reproduce inside of a host. Hosts can collect resources and invest them into defense against their symbionts or donation to their symbionts.',
+        bodyText: 'The symbionts are endosymbionts, meaning they live and reproduce inside of a host. Hosts can either collect resources and invest them into defense against their symbionts or donation to their symbionts.',
         imgSrc: './assets/sym-img-1.png',
+        buttonLabels: ['Back', 'Next'],
+        buttonLayout: 'twoContained',
+      },
+      {
+        titleText: 'Symbionts',
+        bodyText: 'Symbionts can then either steal resources from their host or donate resources back.',
+        imgSrc: './assets/sym-img-2-recropped.png',
+        // todo: do a less cropped version of this image
         buttonLabels: ['Back', 'Next'],
         buttonLayout: 'twoContained',
       },
