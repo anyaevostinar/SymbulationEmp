8a989e6a82bb056e46765e8520c509e9cf9d84f8
Alexander Lalejini <amlalejini@gmail.com>
2025-01-30 15:03:04 -0500

    Remove SyncDataMonitor from SGPWorld.h

--- source/sgp_mode/SGPWorld.h
+++ source/sgp_mode/SGPWorld.h
@@ -7,38 +7,9 @@
 #include "emp/Evolve/World_structure.hpp"
 #include "emp/data/DataNode.hpp"
 #include "SGPConfigSetup.h"
+#include "SyncDataMonitor.h"
 
 namespace sgpmode {
-
-/// Helper which synchronizes access to the DataMonitor with a mutex
-template <typename T, emp::data... MODS> class SyncDataMonitor {
-  std::mutex mutex;
-  emp::DataMonitor<T, MODS...> monitor;
-
-public:
-  /**
-   * Input: None
-   *
-   * Output: Reference to the underlying DataMonitor, without synchronization.
-   *
-   * Purpose: Accesses the underlying monitor without synchronization, should
-   * only be used when you're sure there's no multithreading going on.
-   */
-  emp::DataMonitor<T, MODS...> &UnsynchronizedGetMonitor() { return monitor; }
-
-  /**
-   * Input: An action to perform with the DataMonitor.
-   *
-   * Output: None
-   *
-   * Purpose: Calls the provided callback with the DataMonitor while holding the
-   * mutex, releasing it when it returns.
-   */
-  template <typename F> void WithMonitor(F f) {
-    std::lock_guard lock(mutex);
-    f(monitor);
-  }
-};
 
 class SGPWorld : public SymWorld {
 private:
