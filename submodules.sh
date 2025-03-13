84267760aaf0eac63d52c67f7501f486b7849ed1
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2025-03-13 13:45:30 -0400

    Attribute git submodules trick

--- submodules.sh
+++ submodules.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
+# see https://github.blog/open-source/git/get-up-to-speed-with-partial-clone-and-shallow-clone/
 git submodule update --init --filter=blob:none --recursive --single-branch --jobs "$(nproc || echo 1)"
