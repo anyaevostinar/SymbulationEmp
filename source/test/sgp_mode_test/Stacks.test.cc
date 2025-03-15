ef5a0d78f97523f5bb139b87f81a3e9718942c93
John Shea <sheajo@mail.gvsu.edu>
2025-03-15 17:22:46 -0400

    updated refactor unit test for Stacks.h

--- source/test/sgp_mode_test/Stacks.test.cc
+++ source/test/sgp_mode_test/Stacks.test.cc
@@ -1,3 +1,139 @@
+// #include "../../sgp_mode/hardware/Stacks.h"
+#include "../../catch/catch.hpp"
+// #include "emp/base/vector.hpp"
+// #include "emp/base/array.hpp"
+
+// #include <limits>
+// #include <optional>
+
+
+// previous implementation 
+
+
+// TEST_CASE("Stack initialization", "[sgp]") {
+//     sgpmode::Stacks<int> stacks(3);  
+//     REQUIRE(stacks.GetNumStacks() == 3); 
+//     REQUIRE(stacks.GetActiveStack().size() == 0);  
+// }
+
+// TEST_CASE("Push elements onto stack", "[sgp]") {
+//     sgpmode::Stacks<int> stacks(2);  
+//     stacks.SetActive(0); 
+
+//     REQUIRE(stacks.Push(10) == true);  
+//     REQUIRE(stacks.Push(20) == true); 
+
+   
+//     REQUIRE(stacks.GetTop() == 20);  
+
+//     stacks.SetStackLimit(3); 
+//     REQUIRE(stacks.Push(30) == true); 
+//     REQUIRE(stacks.Push(40) == false); 
+
+// TEST_CASE("Pop elements from stack", "[sgp]") {
+//     sgpmode::Stacks<int> stacks(1);  
+//     stacks.SetActive(0);  
+   
+//     stacks.Push(10);
+//     stacks.Push(20);
+//     stacks.Push(30);
+
+   
+//     auto pop_result = stacks.Pop();
+//     REQUIRE(pop_result.has_value() == true);  
+//     REQUIRE(pop_result.value() == 30);  
+
+//     pop_result = stacks.Pop();
+//     REQUIRE(pop_result.has_value() == true); 
+//     REQUIRE(pop_result.value() == 20);  
+
+//     pop_result = stacks.Pop();
+//     REQUIRE(pop_result.has_value() == true);  
+//     REQUIRE(pop_result.value() == 10); 
+
+//     pop_result = stacks.Pop();
+//     REQUIRE(pop_result.has_value() == false);  
+// }
+
+// TEST_CASE("Clear active stack", "[sgp]") {
+//     sgpmode::Stacks<int> stacks(2);  
+//     stacks.SetActive(0);  
+
+   
+//     stacks.Push(10);
+//     stacks.Push(20);
+
+//     stacks.ClearActive();
+    
+//     REQUIRE(stacks.GetActiveStack().size() == 0);
+// }
+
+// TEST_CASE("Change active stack", "[sgp]") {
+//     sgpmode::Stacks<int> stacks(3);  
+
+//     stacks.SetActive(0);
+//     REQUIRE(stacks.GetActiveStack().size() == 0);  
+
+//     stacks.Push(10);
+//     stacks.Push(20);
+
+   
+//     stacks.SetActive(1);
+//     REQUIRE(stacks.GetActiveStack().size() == 0);  
+
+
+//     stacks.Push(30);
+//     stacks.Push(40);
+
+    
+//     REQUIRE(stacks.GetActiveStack().size() == 2);
+//     REQUIRE(stacks.GetActiveStack()[0] == 30);
+//     REQUIRE(stacks.GetActiveStack()[1] == 40);
+
+
+//     stacks.SetActive(0);
+//     REQUIRE(stacks.GetActiveStack().size() == 2);
+//     REQUIRE(stacks.GetActiveStack()[0] == 10);
+//     REQUIRE(stacks.GetActiveStack()[1] == 20);
+// }
+
+// TEST_CASE("Set stack limit and resize stacks", "[sgp]") {
+//     sgpmode::Stacks<int> stacks(2); 
+
+//     stacks.SetStackLimit(2);
+
+   
+//     stacks.SetActive(0);
+//     stacks.Push(10);
+//     stacks.Push(20);
+//     stacks.Push(30);  
+
+//     REQUIRE(stacks.GetActiveStack().size() == 2);  
+//     REQUIRE(stacks.GetActiveStack()[0] == 10);
+//     REQUIRE(stacks.GetActiveStack()[1] == 20);
+// }
+
+// TEST_CASE("Stack pops after limit resize", "[sgp]") {
+//     sgpmode::Stacks<int> stacks(1);  
+//     stacks.SetActive(0);  
+
+  
+//     stacks.SetStackLimit(3);
+//     stacks.Push(10);
+//     stacks.Push(20);
+//     stacks.Push(30);
+    
+
+//     stacks.SetStackLimit(2);
+//     REQUIRE(stacks.GetActiveStack().size() == 2);  
+
+//     REQUIRE(stacks.GetActiveStack()[0] == 10);
+//     REQUIRE(stacks.GetActiveStack()[1] == 20);  
+// }
+
+
+
+/// updated implementation: 
 #include "../../sgp_mode/hardware/Stacks.h"
 #include "../../catch/catch.hpp"
 #include "emp/base/vector.hpp"
@@ -5,7 +141,6 @@
 
 #include <limits>
 #include <optional>
-
 
 TEST_CASE("Stack initialization", "[sgp]") {
     sgpmode::Stacks<int> stacks(3);  
@@ -20,12 +155,14 @@ TEST_CASE("Push elements onto stack", "[sgp]") {
     REQUIRE(stacks.Push(10) == true);  
     REQUIRE(stacks.Push(20) == true); 
 
-   
+    // Verify top of the stack
+    REQUIRE(stacks.GetTop() == true);
     REQUIRE(stacks.GetTop() == 20);  
 
     stacks.SetStackLimit(3); 
     REQUIRE(stacks.Push(30) == true); 
     REQUIRE(stacks.Push(40) == false); 
+}
 
 TEST_CASE("Pop elements from stack", "[sgp]") {
     sgpmode::Stacks<int> stacks(1);  
@@ -35,7 +172,7 @@ TEST_CASE("Pop elements from stack", "[sgp]") {
     stacks.Push(20);
     stacks.Push(30);
 
-   
+    // Pop and check top element
     auto pop_result = stacks.Pop();
     REQUIRE(pop_result.has_value() == true);  
     REQUIRE(pop_result.value() == 30);  
@@ -56,7 +193,6 @@ TEST_CASE("Clear active stack", "[sgp]") {
     sgpmode::Stacks<int> stacks(2);  
     stacks.SetActive(0);  
 
-   
     stacks.Push(10);
     stacks.Push(20);
 
@@ -74,19 +210,15 @@ TEST_CASE("Change active stack", "[sgp]") {
     stacks.Push(10);
     stacks.Push(20);
 
-   
     stacks.SetActive(1);
     REQUIRE(stacks.GetActiveStack().size() == 0);  
-
 
     stacks.Push(30);
     stacks.Push(40);
 
-    
     REQUIRE(stacks.GetActiveStack().size() == 2);
     REQUIRE(stacks.GetActiveStack()[0] == 30);
     REQUIRE(stacks.GetActiveStack()[1] == 40);
-
 
     stacks.SetActive(0);
     REQUIRE(stacks.GetActiveStack().size() == 2);
@@ -99,12 +231,12 @@ TEST_CASE("Set stack limit and resize stacks", "[sgp]") {
 
     stacks.SetStackLimit(2);
 
-   
     stacks.SetActive(0);
     stacks.Push(10);
     stacks.Push(20);
     stacks.Push(30);  
 
+    // Verify stack size limit
     REQUIRE(stacks.GetActiveStack().size() == 2);  
     REQUIRE(stacks.GetActiveStack()[0] == 10);
     REQUIRE(stacks.GetActiveStack()[1] == 20);
@@ -114,13 +246,12 @@ TEST_CASE("Stack pops after limit resize", "[sgp]") {
     sgpmode::Stacks<int> stacks(1);  
     stacks.SetActive(0);  
 
-  
     stacks.SetStackLimit(3);
     stacks.Push(10);
     stacks.Push(20);
     stacks.Push(30);
-    
 
+    // Resize the stack limit and verify
     stacks.SetStackLimit(2);
     REQUIRE(stacks.GetActiveStack().size() == 2);  
 
@@ -128,20 +259,3 @@ TEST_CASE("Stack pops after limit resize", "[sgp]") {
     REQUIRE(stacks.GetActiveStack()[1] == 20);  
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-// TEST_CASE("SetStackLimit", "[sgp]") {
-
-// }
-
-// sgpmode::Stacks<uint32> hello(5);
