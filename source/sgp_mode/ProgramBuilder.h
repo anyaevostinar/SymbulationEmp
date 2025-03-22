efcaace9b64040a6b0975f8fe192c0b3fb06daa0
Alexander Lalejini <amlalejini@gmail.com>
2025-03-22 14:32:47 -0400

    Re-add add task functions to refactored program builder

--- source/sgp_mode/ProgramBuilder.h
+++ source/sgp_mode/ProgramBuilder.h
@@ -24,10 +24,9 @@ public:
 protected:
   // Can add extra flexibility in future to configure
   // what instructions to use (e.g., different IO instructions, etc)
-  std::string nand_op_name = inst::Nand::name();
-  /* TODO - fix this once IO instruction is implemented!!! */
-  std::string io_op_name = inst::IO::name();
-  // std::string
+  uint8_t nand_op = Library::GetOpCode(inst::Nand::name());
+  uint8_t io_op = Library::GetOpCode(inst::IO::name());
+  uint8_t repro_op = Library::GetOpCode(inst::Reproduce::name());
 
   tag_t start_tag;
 
@@ -43,8 +42,26 @@ protected:
     uint8_t arg2 = 0,
     tag_t tag={}
   ) {
+    AddInst(
+      program,
+      Library::GetOpCode(op_name),
+      arg0,
+      arg1,
+      arg2,
+      tag
+    );
+  }
+
+  void AddInst(
+    program_t& program,
+    uint8_t opcode,
+    uint8_t arg0 = 0,
+    uint8_t arg1 = 0,
+    uint8_t arg2 = 0,
+    tag_t tag={}
+  ) {
     inst_t inst;
-    inst.op_code = Library::GetOpCode(op_name);
+    inst.op_code = opcode;
     inst.args = {arg0, arg1, arg2};
     inst.tag = tag;
     program.emplace_back(inst);
@@ -56,6 +73,14 @@ protected:
     tag_t tag
   ) {
     AddInst(program, op_name, 0, 0, 0, tag);
+  }
+
+  void AddInst(
+    program_t& program,
+    uint8_t opcode,
+    tag_t tag
+  ) {
+    AddInst(program, opcode, 0, 0, 0, tag);
   }
 
 public:
@@ -71,20 +96,200 @@ public:
     start_tag = tag;
   }
 
-  void AddTask_Not(
-    program_t& program
-  ) {
-    // sharedio   r0
-    // nand       r0, r0, r0
-    // sharedio   r0
-    AddInst(program, io_op_name);
-    AddInst(program, nand_op_name);
-    AddInst(program, io_op_name);
+  // Allow io opcode to be configured in case we need to change to a special/different
+  //  io instruction.
+  void SetIOInst(uint8_t opcode) {
+    emp_assert(opcode < Library::GetSize());
+    io_op = opcode;
   }
 
-  program_t CreateNotProgram(
-    size_t length
+  // Allow reproduce opcode to be configured in case we need to change to a special/different
+  //  reproduce instruction.
+  void SetReproduceInst(uint8_t opcode) {
+    emp_assert(opcode < Library::GetSize());
+    repro_op = opcode;
+  }
+
+  // Allow nand opcode to be configured in case we need to change to a special/different
+  //  nand instruction.
+  void SetNandInst(uint8_t opcode) {
+    emp_assert(opcode < Library::GetSize());
+    nand_op = opcode;
+  }
+
+  void AddTask_Not(program_t& program) {
+    // io   r0
+    // nand r0, r0, r0
+    // io   r0
+    AddInst(program, io_op);
+    AddInst(program, nand_op);
+    AddInst(program, io_op);
+    // Rectify program in case any of these instructions have been disabled
+    program.Rectify(rectifier);
+  }
+
+  void AddTask_Nand(program_t& program) {
+    // io   r0
+    // io   r1
+    // nand r0, r1, r0
+    // io   r0
+    AddInst(program, io_op);
+    AddInst(program, io_op, 1);
+    AddInst(program, nand_op, 0, 1, 0);
+    AddInst(program, io_op);
+    // Rectify program in case any of these instructions have been disabled
+    program.Rectify(rectifier);
+  }
+
+  void AddTask_And(
+    program_t& program
   ) {
+    // ~(a nand b)
+    // io   r0
+    // io   r1
+    // nand r0, r1, r0
+    // nand r0, r0, r0
+    // io   r0
+    AddInst(program, io_op);
+    AddInst(program, io_op, 1);
+    AddInst(program, nand_op, 0, 1, 0);
+    AddInst(program, nand_op);
+    AddInst(program, io_op);
+    // Rectify program in case any of these instructions have been disabled
+    program.Rectify(rectifier);
+  }
+
+  void AddTask_OrNot(program_t& program) {
+    // (~a) nand b
+    // io   r0
+    // io   r1
+    // nand r0, r0, r0
+    // nand r0, r1, r0
+    // io   r0
+    AddInst(program, io_op);
+    AddInst(program, io_op, 1);
+    AddInst(program, nand_op);
+    AddInst(program, nand_op, 0, 1, 0);
+    AddInst(program, io_op);
+    // Rectify program in case any of these instructions have been disabled
+    program.Rectify(rectifier);
+  }
+
+  void AddTask_Or(program_t& program) {
+    // (~a) nand (~b)
+    // io   r0
+    // io   r1
+    // nand r0, r0, r0
+    // nand r1, r1, r1
+    // nand r0, r1, r0
+    // io   r0
+    AddInst(program, io_op);
+    AddInst(program, io_op, 1);
+    AddInst(program, nand_op, 0, 0, 0);
+    AddInst(program, nand_op, 1, 1, 1);
+    AddInst(program, nand_op, 0, 1, 0);
+    AddInst(program, io_op);
+    // Rectify program in case any of these instructions have been disabled
+    program.Rectify(rectifier);
+  }
+
+  void AddTask_AndNot(program_t& program) {
+    // ~(a nand (~b))
+    // io   r0
+    // io   r1
+    // nand r1, r1, r1
+    // nand r0, r1, r0
+    // nand r0, r0, r0
+    // io   r0
+    AddInst(program, io_op);
+    AddInst(program, io_op, 1);
+    AddInst(program, nand_op, 1, 1, 1);
+    AddInst(program, nand_op, 0, 1, 0);
+    AddInst(program, nand_op, 0, 0, 0);
+    AddInst(program, io_op);
+    // Rectify program in case any of these instructions have been disabled
+    program.Rectify(rectifier);
+  }
+
+  void AddTask_Nor(program_t& program) {
+    // ~((~a) nand (~b))
+    // io   r0
+    // io   r1
+    // nand r0, r0, r0
+    // nand r1, r1, r1
+    // nand r0, r1, r0
+    // nand r0, r0, r0
+    // io   r0
+    AddInst(io_op);
+    AddInst(io_op, 1);
+    AddInst(nand_op, 0, 0, 0);
+    AddInst(nand_op, 1, 1, 1);
+    AddInst(nand_op, 0, 1, 0);
+    AddInst(nand_op, 0, 0, 0);
+    AddInst(io_op);
+    // Rectify program in case any of these instructions have been disabled
+    program.Rectify(rectifier);
+  }
+
+  void AddTask_Xor(program_t& program) {
+    // (a & ~b) | (~a & b) --> (a nand ~b) nand (~a nand b)
+    // io   r0
+    // io   r1
+    //
+    // nand r3, r1, r1
+    // nand r3, r3, r0
+    //
+    // nand r2, r0, r0
+    // nand r2, r2, r1
+    //
+    // nand r0, r2, r3
+    // io   r0
+    AddInst(program, io_op);
+    AddInst(program, io_op, 1);
+
+    AddInst(program, nand_op, 3, 1, 1);
+    AddInst(program, nand_op, 3, 3, 0);
+
+    AddInst(program, nand_op, 2, 0, 0);
+    AddInst(program, nand_op, 2, 2, 1);
+
+    AddInst(program, nand_op, 0, 2, 3);
+    AddInst(io_op);
+    // Rectify program in case any of these instructions have been disabled
+    program.Rectify(rectifier);
+  }
+
+  void AddTask_Equ(program_t& program) {
+    // ~(a ^ b)
+    // io   r0
+    // io   r1
+    //
+    // nand r3, r1, r1
+    // nand r3, r3, r0
+    //
+    // nand r2, r0, r0
+    // nand r2, r2, r1
+    //
+    // nand r0, r2, r3
+    // nand r0, r0, r0
+    // io   r0
+    AddInst(program, io_op);
+    AddInst(program, io_op, 1);
+
+    AddInst(program, nand_op, 3, 1, 1);
+    AddInst(program, nand_op, 3, 3, 0);
+
+    AddInst(program, nand_op, 2, 0, 0);
+    AddInst(program, nand_op, 2, 2, 1);
+
+    AddInst(program, nand_op, 0, 2, 3);
+    AddInst(program, nand_op, 0, 0, 0);
+    AddInst(program, io_op);
+    // Rectify program in case any of these instructions have been disabled
+    program.Rectify(rectifier);
+  }
+
+  program_t CreateNotProgram(size_t length) {
     program_t program; // Create empty program
     // Add start anchor
     AddInst(
@@ -97,15 +302,56 @@ public:
     // Nop filler is length minus current size + repro instructions
     // const size_t nop_filler = length - (program.size() + 1);
     program.resize(length - 1);
-    AddInst(program, "Reproduce");
+    AddInst(program, repro_op);
     // Remove any deleted instructions
     program.Rectify(rectifier);
     return program;
   }
 
+  program_t CreateRandomProgram(size_t length) {
+    return program_t(length);
+  }
+
+  program_t CreateReproProgram(size_t length) {
+    program_t program;
+    // Add start anchor
+    AddInst(
+      program,
+      "Global Anchor",
+      start_tag
+    );
+    // Add Nop filler
+    program.resize(length - 1);
+    AddInst(program, repro_op);
+    // Remove any deleted instructions
+    program.Rectify(rectifier);
+    return program;
+  }
+
+  program_t CreateNotNandProgram(size_t length) {
+    program_t program; // Create empty program
+    // Add start anchor
+    AddInst(
+      program,
+      "Global Anchor",
+      start_tag
+    );
+    // Add not instruction
+    AddTask_Not(program);  // Add not task
+    AddTask_Nand(program); // Add nand task
+    // Nop filler is length minus current size + repro instructions
+    // const size_t nop_filler = length - (program.size() + 1);
+    program.resize(length - 1);
+    AddInst(program, repro_op);
+    // Remove any deleted instructions
+    program.Rectify(rectifier);
+    return program;
+  }
+
+  // NOTE - program builder doesn't need to know about mutualist start vs parasite start
+  //        World has config details, so world should be responsible for knowing
+  //        what a start program is.
+
 };
-
-
-// TODO - re-implement program builder
 
 }
