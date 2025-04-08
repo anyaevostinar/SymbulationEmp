b5f0c0cb833bc48e139e99702dcc767202de4a63
John Shea <sheajo@mail.gvsu.edu>
2025-04-08 16:43:24 -0400

    updated GetTop function with ternary operator within STacks.h

--- source/sgp_mode/hardware/Stacks.h
+++ source/sgp_mode/hardware/Stacks.h
@@ -96,8 +96,8 @@ public:
 
   // Return the top element of the active stack.
   std::optional<T> GetTop() const {
-    return (stacks[active_stack].size() > 0);
-      stacks[active_stack].back();
+    return (stacks[active_stack].size() > 0) ?
+      stacks[active_stack].back() :
       std::nullopt;
   }
 
