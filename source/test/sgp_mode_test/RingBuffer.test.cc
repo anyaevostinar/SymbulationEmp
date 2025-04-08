0004f43a9d4601a6062b1c0bed234a93f486e58e
John Shea <sheajo@mail.gvsu.edu>
2025-04-08 16:50:10 -0400

    updated push unit tests within RingBuffer.test.cc, removed redundant tests, and deleted original IORingBuffertest.cc

--- source/test/sgp_mode_test/RingBuffer.test.cc
+++ source/test/sgp_mode_test/RingBuffer.test.cc
@@ -1,86 +1,6 @@
 #include "../../sgp_mode/hardware/RingBuffer.h"
 #include "../../catch/catch.hpp"
 
-
-/// previous implementation
-
-// TEST_CASE("IORingBuffer Push", "[sgp]") {
-//     sgpmode::IORingBuffer<int, 3> buffer;
-
-//     SECTION("Push Adds Elements Correctly") {
-//         buffer.push(1);
-//         buffer.push(2);
-//         buffer.push(3);
-
-//         REQUIRE(buffer[0] == 1);
-//         REQUIRE(buffer[1] == 2);
-//         REQUIRE(buffer[2] == 3);
-//     }
-
-//     SECTION("Push Overwrites the Oldest Element When Full") {
-//         buffer.push(1);
-//         buffer.push(2);
-//         buffer.push(3);
-//         buffer.push(4);
-
-//         REQUIRE(buffer[0] == 2); 
-//         REQUIRE(buffer[1] == 3);
-//         REQUIRE(buffer[2] == 4);
-//     }
-// }
-
-// TEST_CASE("IORingBuffer Indexing", "[sgp]") {
-//     sgpmode::IORingBuffer<int, 5> buffer;
-
-//     SECTION("Indexing Accesses Correct Element") {
-//         buffer.push(10);
-//         buffer.push(20);
-//         buffer.push(30);
-
-//         REQUIRE(buffer[0] == 10);
-//         REQUIRE(buffer[1] == 20);
-//         REQUIRE(buffer[2] == 30);
-//     }
-
-//     SECTION("Indexing Wraps Correctly") {
-//         buffer.push(1);
-//         buffer.push(2);
-//         buffer.push(3);
-//         buffer.push(4);
-//         buffer.push(5);
-
-//         REQUIRE(buffer[0] == 1);
-//         REQUIRE(buffer[1] == 2);
-//         REQUIRE(buffer[2] == 3);
-//         REQUIRE(buffer[3] == 4);
-//         REQUIRE(buffer[4] == 5);
-//         buffer.push(6); 
-//         REQUIRE(buffer[0] == 2);
-//         REQUIRE(buffer[1] == 3);
-//         REQUIRE(buffer[2] == 4);
-//         REQUIRE(buffer[3] == 5);
-//         REQUIRE(buffer[4] == 6);
-//     }
-// }
-
-// TEST_CASE("IORingBuffer Reset", "[sgp]") {
-//     sgpmode::IORingBuffer<int, 4> buffer(7); 
-
-//     SECTION("Reset with New Value") {
-//         buffer.Reset(0);
-//         for (size_t i = 0; i < buffer.size(); ++i) {
-//             REQUIRE(buffer[i] == 0);
-//         }
-//     }
-
-//     SECTION("Reset to a Different Value and Verify") {
-//         buffer.Reset(42);
-//         for (size_t i = 0; i < buffer.size(); ++i) {
-//             REQUIRE(buffer[i] == 42);
-//         }
-//     }
-// }
-
 /// new implementation 
 
 TEST_CASE("RingBuffer initialization", "[sgp]") {
@@ -105,7 +25,21 @@ TEST_CASE("Push elements into RingBuffer", "[sgp]") {
     buffer.push(40); 
     REQUIRE(buffer[0] == 40);  
     REQUIRE(buffer[1] == 20); 
-    REQUIRE(buffer[2] == 30);  
+    REQUIRE(buffer[2] == 30);
+
+    // additional pushing 
+
+    buffer.push(10);
+    REQUIRE(buffer[0] == 10);  
+    REQUIRE(buffer[1] == 40); 
+    REQUIRE(buffer[2] == 20);
+
+    buffer.push(50);
+    REQUIRE(buffer[0] == 50);  
+    REQUIRE(buffer[1] == 10); 
+    REQUIRE(buffer[2] == 40);
+
+
 }
 
 TEST_CASE("Read from RingBuffer", "[sgp]") {
@@ -122,22 +56,6 @@ TEST_CASE("Read from RingBuffer", "[sgp]") {
     
     buffer.push(40);  
     REQUIRE(buffer.read() == 40); 
-}
-
-TEST_CASE("RingBuffer with wrap-around behavior", "[sgp]") {
-    sgpmode::RingBuffer<int> buffer(3, 0);  
-
-    buffer.push(10);
-    buffer.push(20);
-    buffer.push(30);
-
-    
-    buffer.push(40); 
-
-   
-    REQUIRE(buffer[0] == 40);  
-    REQUIRE(buffer[1] == 20);  
-    REQUIRE(buffer[2] == 30);  
 }
 
 TEST_CASE("Reset RingBuffer", "[sgp]") {
