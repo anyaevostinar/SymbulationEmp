e2e508ce347848a08b17d4bbef1745a15638bc5a
John Shea <sheajo@mail.gvsu.edu>
2025-04-09 00:12:25 -0400

    updated utils.h unit tests for merge

--- source/test/sgp_mode_test/utils.test.cc
+++ source/test/sgp_mode_test/utils.test.cc
@@ -1,61 +1,3 @@
-// #include "../../source/sgp_mode/utils.h"
-// #include "../../catch/catch.hpp"
-
-// previous implementation
-
-// TEST_CASE("AnyMatch Test", "[sgp]") {
-    
-//     SECTION("Matching bits", "[sgp]") {
-//         emp::BitSet<3> b1("000");
-//         emp::BitSet<3> b2("000"); 
-//         REQUIRE(sgpmode::utils::AnyMatch(b1, b2) == true);
-//   }
-//     SECTION("No matching bits", "[sgp]") {
-//         emp::BitSet<3> bits_a("001");
-//         emp::BitSet<3> bits_b("000"); 
-//         REQUIRE(sgpmode::utils::AnyMatch(bits_a, bits_b) == false);
-//   }
-// }
-
-// TEST_CASE("SimpleMatchCoeff Test", "[sgp]") {
-//   SECTION("Matching bits count", "[sgp]") {
-//     emp::BitSet<3> bits_a("101");
-//     emp::BitSet<3> bits_b("101");
-//     REQUIRE(sgpmode::utils::SimpleMatchCoeff(bits_a, bits_b) == 1);  
-//   }
-
-//   SECTION("No matching bits", "[sgp]") {
-//     emp::BitSet<3> bits_a("101");
-//     emp::BitSet<3> bits_b("010");
-//     REQUIRE(sgpmode::utils::SimpleMatchCoeff(bits_a, bits_b) == 0);  
-//   }
-// }
-
-// TEST_CASE("ResizeClear Test","[sgp]") {
-//   emp::BitVector bits(10, 5);  
-  
-//   SECTION("Resize and clear", "[sgp]") {
-//     sgpmode::utils::ResizeClear(bits, 5);  
-//     REQUIRE(bits.size() == 5); 
-//     REQUIRE(bits.CountOnes() == 0);  
-//   }
-// }
-
-// TEST_CASE("ResizeFill Test", "[sgp]") {
-//   std::vector<int> container = {1, 2, 3, 4, 5};
-  
-//   SECTION("Resize and fill with value", "[sgp]") {
-//     sgpmode::utils::ResizeFill(container, 7, 0);  
-//     REQUIRE(container.size() == 7);  
-//     REQUIRE(container[6] == 0); 
-//   }
-
-//   SECTION("Resize and fill with another value", "[sgp]") {
-//     sgpmode::utils::ResizeFill(container, 7, 10);  
-//     REQUIRE(container[6] == 10);  
-//   }
-// }
-
 
 /// updated implementation of unit tests for refactor: 
 #include "../../source/sgp_mode/utils.h"
@@ -64,14 +6,26 @@
 
 TEST_CASE("AnyMatch Test", "[sgp]") {
     
-    SECTION("Matching bits", "[sgp]") {
+    SECTION("Matching bits - all", "[sgp]") {
         emp::BitSet<3> b1("000");
         emp::BitSet<3> b2("000"); 
         REQUIRE(sgpmode::utils::AnyMatch(b1, b2) == true);
     }
-  
-    SECTION("No matching bits", "[sgp]") {
+
+    SECTION("No matching bits - two ", "[sgp]") {
         emp::BitSet<3> bits_a("001");
+        emp::BitSet<3> bits_b("000"); 
+        REQUIRE(sgpmode::utils::AnyMatch(bits_a, bits_b) == true);
+    }
+
+    SECTION("No matching bits - one ", "[sgp]") {
+        emp::BitSet<3> bits_a("110");
+        emp::BitSet<3> bits_b("000"); 
+        REQUIRE(sgpmode::utils::AnyMatch(bits_a, bits_b) == true);
+    }
+
+    SECTION("No matching bits", "[sgp]") {
+        emp::BitSet<3> bits_a("111");
         emp::BitSet<3> bits_b("000"); 
         REQUIRE(sgpmode::utils::AnyMatch(bits_a, bits_b) == false);
     }
@@ -79,9 +33,21 @@ TEST_CASE("AnyMatch Test", "[sgp]") {
 
 TEST_CASE("SimpleMatchCoeff Test", "[sgp]") {
   
-    SECTION("Matching bits count", "[sgp]") {
+    SECTION("Matching bits count - all", "[sgp]") {
         emp::BitSet<3> bits_a("101");
         emp::BitSet<3> bits_b("101");
+        REQUIRE(sgpmode::utils::SimpleMatchCoeff(bits_a, bits_b) == 3);  
+    }
+
+    SECTION("Matching bits count - 2", "[sgp]") {
+        emp::BitSet<3> bits_a("111");
+        emp::BitSet<3> bits_b("101");
+        REQUIRE(sgpmode::utils::SimpleMatchCoeff(bits_a, bits_b) == 2);  
+    }
+
+    SECTION("Matching bits count - 1", "[sgp]") {
+        emp::BitSet<3> bits_a("111");
+        emp::BitSet<3> bits_b("100");
         REQUIRE(sgpmode::utils::SimpleMatchCoeff(bits_a, bits_b) == 2);  
     }
 
@@ -92,8 +58,10 @@ TEST_CASE("SimpleMatchCoeff Test", "[sgp]") {
     }
 }
 
-TEST_CASE("ResizeClear Test", "[sgp]") {
-    emp::BitVector bits(10, 5);  
+TEST_CASE("ResizeClear Test - no ones", "[sgp]") {
+    emp::BitVector bits(10, 5); 
+    REQUIRE(bits.CountOnes() == 0); 
+ 
     
     SECTION("Resize and clear", "[sgp]") {
         sgpmode::utils::ResizeClear(bits, 5);  
@@ -103,7 +71,8 @@ TEST_CASE("ResizeClear Test", "[sgp]") {
 }
 
 TEST_CASE("ResizeFill Test", "[sgp]") {
-    std::vector<int> container = {1, 2, 3, 4, 5};
+    std::vector<int> container = {1,2,3,4,5};
+
     
     SECTION("Resize and fill with value", "[sgp]") {
         sgpmode::utils::ResizeFill(container, 7, 0);  
