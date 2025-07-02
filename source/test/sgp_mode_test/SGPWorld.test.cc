9287092ffd8828af709a72ac371570d1e0450c92
EliasH-E <hillmane@carleton.edu>
2025-07-02 13:52:25 -0500

    Made fixes to tests and folded ONLYNOT test into SGPWorld.test.cc since it is technically testing CheckOutput which is called by SGPWorld

--- source/test/sgp_mode_test/SGPWorld.test.cc
+++ source/test/sgp_mode_test/SGPWorld.test.cc
@@ -138,6 +138,63 @@ TEST_CASE("TaskMatchCheck", "[sgp]") {
 
 }
 
+TEST_CASE("Organisms, without mutation will only recieve credit for NOT operations", "[sgp]") {
+     
+  emp::Random random(1);
+  SymConfigSGP config;
+  config.RANDOM_ANCESTOR(false);
+  config.SEED(2);
+  config.ORGANISM_TYPE(HEALTH);
+  config.STRESS_TYPE(PARASITE);
+  config.MUTATION_RATE(0.0);
+  config.MUTATION_SIZE(0.000);
+  SGPWorld world(random, &config, LogicTasks);
+
+  // Mock Organism to check reproduction
+  class TestOrg : public Organism {
+  public:
+    bool IsHost() override { return true; }
+    void AddPoints(double p) override {}
+    double GetPoints() override { return 0; }
+  };
+
+  TestOrg organism;
+
+  // NOT builder
+  ProgramBuilder builder;
+  (builder.AddNot)();
+  CPU cpu(&organism, &world, builder.Build(100));
+  
+  
+  
+  cpu.RunCPUStep(0, 100);
+  
+  //The result of a AND bitwise operations when one of the inputs, in binary, is all ones will be the other input
+  int all_ones_binary = 4294967295;
+  cpu.state.input_buf.push(all_ones_binary);
+  cpu.RunCPUStep(0, 100);
+  world.Update();
+
+  //Checks both that NOT is being done and no other operations are being done
+  for (auto data : world.GetTaskSet()) {
+    
+      if(data.task.name != "NOT"){
+    
+       REQUIRE(data.n_succeeds_host == 0);
+      }
+      else{
+        REQUIRE(data.n_succeeds_host > 0);
+      }
+    
+  }
+
+  cpu.state.shared_available_dependencies.Delete();
+  cpu.state.used_resources.Delete();
+  cpu.state.internal_environment.Delete();
+
+
+}
+
 TEST_CASE("Ousting is permitted", "[sgp]") {
   emp::Random random(61);
   SymConfigSGP config;
