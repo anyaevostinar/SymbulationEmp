a13847ac4510c4200d87f34e0abe5a4ff68b1588
EliasH-E <hillmane@carleton.edu>
2025-12-04 14:25:22 -0600

    Fixed Error in test by setting SYNERGY to 1

--- source/test/sgp_mode_test/HealthHost.test.cc
+++ source/test/sgp_mode_test/HealthHost.test.cc
@@ -26,7 +26,7 @@ TEST_CASE("Health host with symbiont loses/gains cycle 50% of time", "[sgp]") {
   config.HOST_REPRO_RES(100);
   config.SYM_HORIZ_TRANS_RES(10);
   config.DONATION_STEAL_INST(0);
-
+  config.SYNERGY(1);
   config.OUSTING(1);
 
   SGPWorld world(random, &config, LogicTasks);
@@ -128,6 +128,7 @@ TEST_CASE("Health hosts evolve less NOT with parasites than without", "[sgp]") {
   //TODO: The random number seed doesn't seem to be working, different values for the same seed
 
   SymConfigSGP config;
+  config.SYNERGY(1);
   config.SEED(10);  
   config.INTERACTION_MECHANISM(1); //Health hosts
   config.SYMBIONT_TYPE(1); //Parasites
@@ -204,6 +205,7 @@ TEST_CASE("Health hosts evolve", "[sgp][integration]") {
   config.GRID_X(10);
   config.GRID_Y(100);
   config.HOST_REPRO_RES(20);
+  config.SYNERGY(1);
   size_t world_size = config.GRID_X() * config.GRID_Y();
 
   SGPWorld world(random, &config, LogicTasks);
@@ -248,6 +250,7 @@ TEST_CASE("When DONATION_STEAL_INST is 1 then Symbiont with 'Steal' instruction 
  
   emp::Random random(1);
   SymConfigSGP config;
+  config.SYNERGY(1);
   config.SEED(0);
   config.INTERACTION_MECHANISM(HEALTH);
   config.SYMBIONT_TYPE(1);
@@ -303,6 +306,7 @@ TEST_CASE("When DONATION_STEAL_INST is 1 then Symbiont with 'Donate' instruction
  
   emp::Random random(1);
   SymConfigSGP config;
+  config.SYNERGY(1);
   config.SEED(0);
   config.INTERACTION_MECHANISM(HEALTH);
   config.SYMBIONT_TYPE(0);
