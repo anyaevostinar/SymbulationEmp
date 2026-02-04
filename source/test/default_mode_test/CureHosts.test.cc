a66744924d3cd284d4333fcfdad0c8d627ed64c3
Delaney Kelley <kelleyde@mail.gvsu.edu>
2026-02-03 22:11:48 -0500

    got rid of redundant tests, added tests with a host with multiple syms

--- source/test/default_mode_test/CureHosts.test.cc
+++ source/test/default_mode_test/CureHosts.test.cc
@@ -10,7 +10,7 @@ TEST_CASE("Cure Hosts tests", "[default]"){
     emp::Random random(17);
     SymConfigBase config;
     SymWorld world(random, &config);
-    int pop_size = 5;
+    int pop_size = 3;
 
     emp::vector<emp::Ptr<Organism>> host_vect;
     emp::vector<emp::Ptr<Organism>> sym_vect;
@@ -47,14 +47,6 @@ TEST_CASE("Cure Hosts tests", "[default]"){
           REQUIRE(sym_vect[i]->GetDead() == false);
         }
       }
-      // All syms from world.GetSymPop() are nullptrs
-      THEN("All syms are nullptrs"){
-        // sym_pop[i] still points to an address
-        emp::World<Organism>::pop_t world_syms = world.GetSymPop();
-        for(int i = 0; i < pop_size; i++){
-          REQUIRE(world_syms[i] != nullptr);
-        }
-      }
     } // When (Hosts are not cured)
 
     WHEN("Hosts are cured by calling CureHosts()"){
@@ -67,75 +59,101 @@ TEST_CASE("Cure Hosts tests", "[default]"){
       }
 
       WHEN ("Run an extra update afterwards"){
-        // world.CureHosts();
         // extra update runs through and kills syms
         world.Update();
-        THEN("Syms population is zero"){
-          // sym pop size is still the same. FAILS
-          REQUIRE(world.GetSymPop().size() == 0);
-        }
         THEN("Hosts don't point to syms"){
           for (int i = 0; i < pop_size; i++){
             REQUIRE(host_vect[i]->HasSym() == false);
           }
         }
-        // can't check if syms are set to dead due to segmentation violation signal (syms are deleted nothing to point to)
-        // THEN("syms are set to dead"){
+        // // can't check if syms are set to dead due to segmentation violation signal (syms are deleted nothing to point to)
+        // THEN("syms raise errors"){
         //   for (int i = 0; i < pop_size; i++){
-        //     REQUIRE(sym_vect[i]->GetDead());
+        //     REQUIRE_THROWS_WITH(sym_vect[i]->GetDead(), "Segmentation fault (core dumped)");
         //   }
         // } 
-        // All syms from world.GetSymPop() are nullptrs
-        THEN("Syms are nullptrs"){
-        // sym_pop[i] still points to an address
-          emp::World<Organism>::pop_t world_syms = world.GetSymPop();
-          for(int i = 0; i < pop_size; i++){
-            REQUIRE(world_syms[i] == nullptr);
-          }
-        }
+      
       } // WHEN (run update)
       } // WHEN(Hosts are cured)
 
     WHEN("Host are cured while experiment is running"){
       config.CURE(1);
       int num_updates = 1;
-      int total_updates = 4;
+      int total_updates = 3;
       config.CURE_UPDATES(num_updates);
       config.UPDATES(total_updates);
       world.RunExperiment(true);
+      auto world_pop = world.GetPop();
 
-      THEN("Sym population is 0"){
-        // FAILS
-        REQUIRE(world.GetSymPop().size() == 0);
-      }
       THEN("Hosts do not have syms"){
         for (int i = 0; i < pop_size; i++){
-          REQUIRE(host_vect[i]->HasSym() == false);
+          REQUIRE(world_pop[i]->HasSym() == false);
         }
       }
     } // WHEN (runExperiment)
 
-    // WHEN("Simulating logic in RunExperiment"){
-    //   config.CURE(1);
-    //   int num_updates = 1;
-    //   int total_updates = 4;
-    //   config.CURE_UPDATES(num_updates);
-    //   config.UPDATES(total_updates);
-    //   emp::World<Organism>::pop_t world_syms = world.GetSymPop();
+    WHEN("Simulating logic in RunExperiment"){
+      config.CURE(1);
+      int num_updates = 1;
+      int total_updates = 4;
+      config.CURE_UPDATES(num_updates);
+      config.UPDATES(total_updates);
       
-    //   for(int j = 0; j < total_updates; j++) {
-    //     if (config.CURE() && j == num_updates) {
-    //       world.CureHosts();
-    //     }
-    //     if (j < num_updates + 1) { // syms alive, still point to address
-    //       REQUIRE(world_syms[j] != nullptr);
-    //     }
-    //     if (j > num_updates + 1) { // syms killed, don't point to address
-    //       REQUIRE(world_syms[j] == nullptr);
-    //     } 
-    //     world.Update();
-    //     } //outer for 
-    //   } // WHEN (simulation of run experiement )
+      for(int j = 0; j < total_updates; j++) {
+        if (config.CURE() && j == num_updates) {
+          world.CureHosts();
+        }
+        // num_updates + 1, since it takes an update for deaths to process
+        if (j < num_updates + 1) { // syms alive, hosts have pointers to syms
+          for (int i = 0; i < pop_size; i++){
+            REQUIRE(host_vect[i]->HasSym());
+          }
+        }
+        if (j > num_updates + 1) { // syms dead, hosts don't have pointers to syms
+          for (int i = 0; i < pop_size; i++){
+            REQUIRE(host_vect[i]->HasSym() == false);
+          }
+        } 
+        world.Update();
+        } //outer for 
+      } // WHEN (simulation of run experiement )
+
   } //GIVEN
 } //TEST_CASE
 
+TEST_CASE("Curing a host with multiple symbionts"){
+  GIVEN("a world"){
+    // make world
+    emp::Random random(17);
+    SymConfigBase config;
+    SymWorld world(random, &config);
+    // setting up hosts
+    emp::Ptr<Host> host_twos;
+    host_twos.New(&random, &world, &config);
+    world.InjectHost(host_twos);
+    // setting up symbionts
+    emp::vector<emp::Ptr<Organism>> syms;
+    emp::Ptr<Symbiont> sym1;
+    sym1.New(&random, &world, &config);
+    emp::Ptr<Symbiont> sym2;
+    sym2.New(&random, &world, &config); 
+    syms.push_back(sym1);
+    syms.push_back(sym2);
+    host_twos->SetSymbionts(syms);
+        
+      WHEN("Host is not cured"){  
+        THEN("Host has two symbionts"){
+          REQUIRE(host_twos->GetSymbionts().size() == 2);
+          REQUIRE(host_twos->HasSym());
+        }
+      }
+      WHEN("Host is cured"){
+        THEN("Host doesn't have any symbionts after being cured"){
+          world.CureHosts();
+          world.Update();
+          REQUIRE(host_twos->HasSym() == false);
+        }
+      }
+
+  } // GIVEN
+} // TEST_CASE
