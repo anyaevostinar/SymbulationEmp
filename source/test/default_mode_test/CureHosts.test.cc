b750eb7f231de71fb3d525c86419ccb1998e77f8
Delaney Kelley <kelleyde@mail.gvsu.edu>
2026-02-03 21:12:16 -0500

    CureHosts test case

--- source/test/default_mode_test/CureHosts.test.cc
+++ source/test/default_mode_test/CureHosts.test.cc
@@ -4,228 +4,138 @@
 #include "../../Empirical/include/emp/math/Random.hpp"
 
 
-TEST_CASE("CureHost", "[default]"){
+TEST_CASE("Cure Hosts tests", "[default]"){
   GIVEN("a world") {
     // make world
     emp::Random random(17);
     SymConfigBase config;
     SymWorld world(random, &config);
-    int int_val = 0;
+    int pop_size = 5;
 
-    // host and sym 1
-    emp::Ptr<Organism> host1 = emp::NewPtr<Host>(&random, &world, &config, int_val);
-    world.AddOrgAt(host1, 0);
-    emp::Ptr<Organism> sym1 = emp::NewPtr<Symbiont>(&random, &world, &config, int_val);
-    host1->AddSymbiont(sym1);
+    emp::vector<emp::Ptr<Organism>> host_vect;
+    emp::vector<emp::Ptr<Organism>> sym_vect;
 
-    // host and sym 2
-    emp::Ptr<Organism> host2 = emp::NewPtr<Host>(&random, &world, &config, int_val);
-    world.AddOrgAt(host2, 1);
-    emp::Ptr<Organism> sym2 = emp::NewPtr<Symbiont>(&random, &world, &config, int_val);
-    host2->AddSymbiont(sym2);
-
-    // host and sym 3
-    emp::Ptr<Organism> host3 = emp::NewPtr<Host>(&random, &world, &config, int_val);
-    world.AddOrgAt(host3, 2);
-    emp::Ptr<Organism> sym3 = emp::NewPtr<Symbiont>(&random, &world, &config, int_val);
-    host3->AddSymbiont(sym3);
-
-    WHEN ('Host are not cured') {
-      THEN ('Hosts still have symbionts') {
-        REQUIRE(host1->HasSym());
-        REQUIRE(host2->HasSym());
-        REQUIRE(host3->HasSym());
-      }
-    }
-
-    WHEN ('Call CureHost()') {
-      world.CureHosts();
-      THEN ('Hosts are cure') {
-        REQUIRE(host1->HasSym() == false);
-        REQUIRE(host2->HasSym() == false);
-        REQUIRE(host3->HasSym() == false);
-      }
-    }
-  } //GIVEN
-} //TEST_CASE
-
-TEST_CASE("CureHostIntegration", "[default]") {
-  GIVEN("a world") {
-    emp::Random random(17);
-    SymConfigBase config;
-    SymWorld world(random, &config);
-    int int_val = 0;
-    // int total_updates = 10;
-    int num_updates = 1;
-    config.CURE(1);
-    config.CURE_UPDATES(num_updates);
-    config.UPDATES(2);
-
-    // Adding hosts and syms
-    // host and sym 1
-    emp::Ptr<Organism> host1 = emp::NewPtr<Host>(&random, &world, &config, int_val);
-    world.AddOrgAt(host1, 0);
-    emp::Ptr<Organism> sym1 = emp::NewPtr<Symbiont>(&random, &world, &config, int_val);
-    host1->AddSymbiont(sym1);
-
-    // host and sym 2
-    emp::Ptr<Organism> host2 = emp::NewPtr<Host>(&random, &world, &config, int_val);
-    world.AddOrgAt(host2, 1);
-    emp::Ptr<Organism> sym2 = emp::NewPtr<Symbiont>(&random, &world, &config, int_val);
-    host2->AddSymbiont(sym2);
-
-    // host and sym 3
-    emp::Ptr<Organism> host3 = emp::NewPtr<Host>(&random, &world, &config, int_val);
-    world.AddOrgAt(host3, 2);
-    emp::Ptr<Organism> sym3 = emp::NewPtr<Symbiont>(&random, &world, &config, int_val);
-    host3->AddSymbiont(sym3);
-
-    WHEN('No experiment run with config.CURE(1)')
-      REQUIRE(host1->HasSym());
-      REQUIRE(host2->HasSym());
-      REQUIRE(host3->HasSym());
-    WHEN("Hosts are cured of all symbionts on a specific update") {
-      //Simulate
-      world.RunExperiment(true);
-      REQUIRE(host1->HasSym() == false);
-      REQUIRE(host2->HasSym() == false);
-      REQUIRE(host3->HasSym() == false);
-    }
-  } //GIVEN
-} //TEST_CASE
-
-
-TEST_CASE("Simulation of RunExperiment CureHosts logic", "[default]"){
-  GIVEN("a world"){
-    emp::Random random(17);
-    SymConfigBase config;
-    SymWorld world(random, &config);
-    
-    // num_updates and total_updates values == or > 100 can cause fail or cause errors
-    int int_val = 0;
-    int num_updates = 3;
-    config.CURE_UPDATES(num_updates);
-    int total_updates = 5; //config.UPDATES
-
-    // Injecting hosts and symbionts into the world
-    emp::Ptr<Organism> host1 = emp::NewPtr<Host>(&random, &world, &config, int_val);
-    world.AddOrgAt(host1, 0);
-    emp::Ptr<Organism> sym1 = emp::NewPtr<Symbiont>(&random, &world, &config, int_val);
-    host1->AddSymbiont(sym1);
-
-    // host and sym 2
-    emp::Ptr<Organism> host2 = emp::NewPtr<Host>(&random, &world, &config, int_val);
-    world.AddOrgAt(host2, 1);
-    emp::Ptr<Organism> sym2 = emp::NewPtr<Symbiont>(&random, &world, &config, int_val);
-    host2->AddSymbiont(sym2);
-
-    // host and sym 3
-    emp::Ptr<Organism> host3 = emp::NewPtr<Host>(&random, &world, &config, int_val);
-    world.AddOrgAt(host3, 2);
-    emp::Ptr<Organism> sym3 = emp::NewPtr<Symbiont>(&random, &world, &config, int_val);
-    host3->AddSymbiont(sym3);
-
-
-    WHEN("Run Updates with config.CURE(1)") {
-      config.CURE(1);
-      for(int i = 0; i < total_updates; i++) {
-        world.Update();
-        if (config.CURE() && i == num_updates) {
-          world.CureHosts();
-        }
-        if (i < num_updates) {
-          REQUIRE(host1->HasSym());
-          REQUIRE(host2->HasSym());
-          REQUIRE(host3->HasSym());
-        }
-        if (i >= num_updates) {
-          REQUIRE(host1->HasSym() == false);
-          REQUIRE(host2->HasSym() == false);
-          REQUIRE(host3->HasSym() == false);
-        }
-      }
-    } //WHEN
-
-    WHEN("CURE(0) and CURE_UPDATES(num_updates > 0)"){
-      config.CURE(0);
-      for(int i = 0; i < total_updates; i++) {
-        world.Update();
-        if (config.CURE() && i == num_updates) {
-          world.CureHosts();
-        }
-        REQUIRE(host1->HasSym());
-        REQUIRE(host2->HasSym());
-        REQUIRE(host3->HasSym());
-      }
-     } //WHEN
-  } //GIVEN
-} //TEST_CASE
-
-TEST_CASE("Adding hosts and symbionts in a loop", "[default]"){
-  GIVEN("a world") {
-    // make world
-    emp::Random random(17);
-    SymConfigBase config;
-    SymWorld world(random, &config);
-    int int_val = 0;
-    int pop_size = 10;
-
+    // Adding hosts and symbionts to the world
     for (int i = 0; i < pop_size; i++) {
-    emp::Ptr<Host> new_org;
-    new_org.New(&random, &world, &config, int_val);
-    world.InjectHost(new_org);
-    emp::Ptr<Symbiont> sym;
-    sym.New(&random, &world, &config, int_val);
-    world.InjectSymbiont(sym);
-  }
-  REQUIRE(world.GetPop().size() == 10);
-  REQUIRE(world.GetSymPop().size() == 10);
+      emp::Ptr<Host> host;
+      host.New(&random, &world, &config);
+      world.InjectHost(host);
+      host_vect.push_back(host);
+      emp::Ptr<Symbiont> sym;
+      sym.New(&random, &world, &config);
+      host->AddSymbiont(sym);
+      sym_vect.push_back(sym);
+    }
+  
+    WHEN("Hosts are not cured"){
+      // Hosts and Sym world pop == pop_size
+      REQUIRE(world.GetPop().size() == pop_size);
+      REQUIRE(world.GetSymPop().size() == pop_size);
+      // hosts have pointer to syms
+      THEN("Hosts have pointers to symbionts"){
+        for (int i = 0; i < pop_size; i++){
+          REQUIRE(host_vect[i]->HasSym());
+        }
+      }
+      THEN("syms have pointers to hosts"){
+      for (int i = 0; i < pop_size; i++){
+        REQUIRE(sym_vect[i]->GetHost() == host_vect[i]);
+      }
+      }
+      THEN("syms aren't set to dead"){
+        for (int i = 0; i < pop_size; i++){
+          REQUIRE(sym_vect[i]->GetDead() == false);
+        }
+      }
+      // All syms from world.GetSymPop() are nullptrs
+      THEN("All syms are nullptrs"){
+        // sym_pop[i] still points to an address
+        emp::World<Organism>::pop_t world_syms = world.GetSymPop();
+        for(int i = 0; i < pop_size; i++){
+          REQUIRE(world_syms[i] != nullptr);
+        }
+      }
+    } // When (Hosts are not cured)
 
+    WHEN("Hosts are cured by calling CureHosts()"){
+      world.CureHosts();
+      // syms and hosts still point to eachother, pop size is the same
+      THEN("All syms are marked dead"){
+        for(int i = 0; i < pop_size; i++){
+          REQUIRE(sym_vect[i]->GetDead());
+        }
+      }
 
+      WHEN ("Run an extra update afterwards"){
+        // world.CureHosts();
+        // extra update runs through and kills syms
+        world.Update();
+        THEN("Syms population is zero"){
+          // sym pop size is still the same. FAILS
+          REQUIRE(world.GetSymPop().size() == 0);
+        }
+        THEN("Hosts don't point to syms"){
+          for (int i = 0; i < pop_size; i++){
+            REQUIRE(host_vect[i]->HasSym() == false);
+          }
+        }
+        // can't check if syms are set to dead due to segmentation violation signal (syms are deleted nothing to point to)
+        // THEN("syms are set to dead"){
+        //   for (int i = 0; i < pop_size; i++){
+        //     REQUIRE(sym_vect[i]->GetDead());
+        //   }
+        // } 
+        // All syms from world.GetSymPop() are nullptrs
+        THEN("Syms are nullptrs"){
+        // sym_pop[i] still points to an address
+          emp::World<Organism>::pop_t world_syms = world.GetSymPop();
+          for(int i = 0; i < pop_size; i++){
+            REQUIRE(world_syms[i] == nullptr);
+          }
+        }
+      } // WHEN (run update)
+      } // WHEN(Hosts are cured)
+
+    WHEN("Host are cured while experiment is running"){
+      config.CURE(1);
+      int num_updates = 1;
+      int total_updates = 4;
+      config.CURE_UPDATES(num_updates);
+      config.UPDATES(total_updates);
+      world.RunExperiment(true);
+
+      THEN("Sym population is 0"){
+        // FAILS
+        REQUIRE(world.GetSymPop().size() == 0);
+      }
+      THEN("Hosts do not have syms"){
+        for (int i = 0; i < pop_size; i++){
+          REQUIRE(host_vect[i]->HasSym() == false);
+        }
+      }
+    } // WHEN (runExperiment)
+
+    // WHEN("Simulating logic in RunExperiment"){
+    //   config.CURE(1);
+    //   int num_updates = 1;
+    //   int total_updates = 4;
+    //   config.CURE_UPDATES(num_updates);
+    //   config.UPDATES(total_updates);
+    //   emp::World<Organism>::pop_t world_syms = world.GetSymPop();
+      
+    //   for(int j = 0; j < total_updates; j++) {
+    //     if (config.CURE() && j == num_updates) {
+    //       world.CureHosts();
+    //     }
+    //     if (j < num_updates + 1) { // syms alive, still point to address
+    //       REQUIRE(world_syms[j] != nullptr);
+    //     }
+    //     if (j > num_updates + 1) { // syms killed, don't point to address
+    //       REQUIRE(world_syms[j] == nullptr);
+    //     } 
+    //     world.Update();
+    //     } //outer for 
+    //   } // WHEN (simulation of run experiement )
   } //GIVEN
 } //TEST_CASE
 
-// TEST_CASE("Problems with CureHosts()", "[default]") {
-//   GIVEN("a world") {
-//     // make world
-//     emp::Random random(17);
-//     SymConfigBase config;
-//     SymWorld world(random, &config);
-//     int int_val = 0;
-
-//     // host and sym 1
-//     emp::Ptr<Organism> host1 = emp::NewPtr<Host>(&random, &world, &config, int_val);
-//     world.AddOrgAt(host1, 0);
-//     emp::Ptr<Organism> sym1 = emp::NewPtr<Symbiont>(&random, &world, &config, int_val);
-//     host1->AddSymbiont(sym1);
-
-//     // host and sym 2
-//     emp::Ptr<Organism> host2 = emp::NewPtr<Host>(&random, &world, &config, int_val);
-//     world.AddOrgAt(host2, 1);
-//     emp::Ptr<Organism> sym2 = emp::NewPtr<Symbiont>(&random, &world, &config, int_val);
-//     host2->AddSymbiont(sym2);
-
-//     // host and sym 3
-//     emp::Ptr<Organism> host3 = emp::NewPtr<Host>(&random, &world, &config, int_val);
-//     world.AddOrgAt(host3, 2);
-//     emp::Ptr<Organism> sym3 = emp::NewPtr<Symbiont>(&random, &world, &config, int_val);
-//     host3->AddSymbiont(sym3);
-
-//     WHEN("CureHosts() is called") {
-//       world.CureHosts();
-//       THEN("All symbionts should be dead") {
-//         REQUIRE(sym1->GetDead());
-//         REQUIRE(sym2->GetDead());
-//         REQUIRE(sym3->GetDead());
-//       }
-//       THEN("Symbiont population should be 0"){
-//         REQUIRE(world.GetSymPop().size() == 0);
-//       }
-//       THEN("Syms shoud not have a host"){
-//         REQUIRE(sym1->GetHost() != host1);
-//         REQUIRE(sym2->GetHost() != host2);
-//         REQUIRE(sym3->GetHost() != host3);
-//       }
-//     }
-//   } //GIVEN
-// } //TEST_CASE
