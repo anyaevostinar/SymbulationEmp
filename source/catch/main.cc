e71ddcbfb37171bf4a4316e088a379607e915161
Anya Vostinar <vostinar@carleton.edu>
2026-05-29 12:32:05 -0500

    adding NutrientMode test file

--- source/catch/main.cc
+++ source/catch/main.cc
@@ -72,6 +72,7 @@
 
 #include "../test/sgp_mode_test/functional_tests/TempChangingEnvironments.test.cc"
 #include "../test/sgp_mode_test/functional_tests/SenseTask_Tasks.test.cc"
+#include "../test/sgp_mode_test/functional_tests/NutrientMode.test.cc"
 
 // Anya's tests
 #include "../test/sgp_mode_test/unit_tests/SGPWorld.test.cc"
